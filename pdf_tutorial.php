c839292b8fbfaf13bbd93a14c7cf4d18ccb7d161
Mats Rappe <walderik@yahoo.se>
2022-12-01 15:14:47 +0100

    BHA! PHP behöver ;

--- pdf_tutorial.php
+++ pdf_tutorial.php
@@ -30,7 +30,7 @@ class TELEGRAM extends FPDF {
     function SetText($text) {
         $this->SetFont('Times','B',16);    # OK är 'Times', 'Arial'
         $this->Cell(40,10,'Hello World!'); # http://www.fpdf.org/en/doc/cell.htm
-        $str = 'ÅÄÖ åäö. &aring;'          # https://stackoverflow.com/questions/3514076/special-characters-in-fpdf-with-php
+        $str = "ÅÄÖ åäö. &aring;";         # https://stackoverflow.com/questions/3514076/special-characters-in-fpdf-with-php
         $str = utf8_decode($str); 
         $this->Cell(60,10,$str,0,1,'C'); # 0 - No border, 1 -  to the beginning of the next line, C - Centrerad
     }
