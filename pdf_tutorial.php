d7f49b5b5d254144d546b5b48e006a67ef593ee4
Mats Rappe <walderik@yahoo.se>
2022-12-01 14:51:03 +0100

    Mer Pdf-trixande

--- pdf_tutorial.php
+++ pdf_tutorial.php
@@ -1,9 +1,15 @@
 <?php
+# Läs mer på http://www.fpdf.org/
+
 require('includes/fpdf185/fpdf.php');
+# $this->MultiCell(0,5,$txt);
 
 $pdf = new FPDF();
+$pdf->SetTitle('Telegram');
+$pdf->SetAuthor('Död mans hand');
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
+$pdf->SetFont('Times','B',16); # OK är 'Times', 'Arial'
 $pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(60,10,'Powered by FPDF.',0,1,'C');
 $pdf->Output();
 ?>
