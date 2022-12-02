19eef681ffbcf26e00499a23474365ce39ac5570
Karin Rappe <walderik@yahoo.se>
2022-12-02 12:44:45 +0100

    Uppdaterat css

--- group_registration.php
+++ group_registration.php
@@ -1,8 +1,7 @@
-<?php 
+<?php
+include_once 'includes/db.inc.php';
 
-	include_once 'includes/db.inc.php';
-
-?> 
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
@@ -10,82 +9,129 @@
 <title>Gruppanmälan DMH 2023</title>
 <link rel="stylesheet" href="includes/registration_system.css">
 </head>
-  <body>
-  		<div id="site-header">
-		<a href="https://dmh.berghemsvanner.se/" rel="home">
-			<img src="images//IMG_1665485583436.png" width="1080" height="237" alt="Död mans hand" />
+<body>
+	<div id="site-header">
+		<a href="https://dmh.berghemsvanner.se/" rel="home"> <img
+			src="images//IMG_1665485583436.png" width="1080" height="237"
+			alt="Död mans hand" />
 		</a>
 	</div>
-	
-			<nav id="primary-navigation" class="site-navigation primary-navigation">
-				<button class="menu-toggle">Primär meny</button>
-				<a class="screen-reader-text skip-link" href="#content">Hoppa till innehåll</a>
-				<div class="menu-huvud-container"><ul id="primary-menu" class="nav-menu"><li id="menu-item-917" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-199 current_page_item menu-item-917"><a href="https://dmh.berghemsvanner.se/" aria-current="page">#199 (ingen rubrik)</a></li>
-<li id="menu-item-906" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-906"><a href="https://dmh.berghemsvanner.se/praktiskt-vinterlajv/">Jullajv: Praktiskt</a></li>
-<li id="menu-item-914" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-914"><a href="https://dmh.berghemsvanner.se/anmalan-till-lajvet/">JULLAJV: ANMÄLAN</a></li>
-<li id="menu-item-907" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-907"><a href="https://dmh.berghemsvanner.se/dod-mans-hand-2022/">HÖSTLAJV 2023</a></li>
-</ul></div>			</nav>
-	<div id="main" class="site-main">
 
-<div id="main-content" class="main-content">
-    <form>
-      
-      <h2> Gruppanmälan !
-        </h2>
-      <p>
-        En grupp är en gruppering av roller som gör något tillsammans på lajvet. Exempelvis en familj på lajvet, en rånarliga eller ett rallarlag.
-      </p>   
-      <h2>Gruppledare</h2> 
-      <p>
-      	Gruppledaren är den som arrangörerna kommer att kontakta när det uppstår frågor kring gruppen.<br>
-        <label for="group_leader_name">Gruppledarens namn:</label><br>
-  		<input type="text" id="group_leader_name" name="group_leader_name"><br>
-        <label for="email">E-post:</label><br>
-  		<input type="email" id="email" name="email"><br>
-      </p>
-      <h2>Information om gruppen</h2> 
-      <p>
-        <label for="group_name">Gruppens namn:</label><br>
-  		<input type="text" id="group_name" name="group_name"><br>
-  		
-        <label for="approximate_number_of_participants">Ungefär hur många gruppmedlemmar kommer ni att bli?:</label><br>
-  		<input type="text" id="approximate_number_of_participants" name="approximate_number_of_participants"><br>
+	<div id="simpleCenter">
+		<form>
 
-        <label for="housing_request">Hur vill ni bo som grupp?:</label><br>
+			<h1>Gruppanmälan</h1>
+			<p>En grupp är en gruppering av roller som gör något tillsammans på
+				lajvet. Exempelvis en familj på lajvet, en rånarliga eller ett
+				rallarlag.</p>
+			<h2>Gruppledare</h2>
+			<p>
+				Gruppledaren är den som arrangörerna kommer att kontakta när det
+				uppstår frågor kring gruppen.
+				<div class="question">
+				<label for="group_leader_name">Gruppledarens
+					namn</label><br> <input type="text" id="group_leader_name"
+					name="group_leader_name" required>
+					</div>
+					<div class="question">
+					<label for="email">E-post</label><br>
+				<input type="email" id="email" name="email" required>
+				</div>
+			</p>
+			
+			
+			<h2>Information om gruppen</h2>
+			
+			
+			<div class="question">
+				<label for="group_name">Gruppens namn</label><br> <input
+					type="text" id="group_name" name="group_name" required>
+			</div>
+			<div class="question">
+				<label for="approximate_number_of_participants">Ungefär hur många
+					gruppmedlemmar kommer ni att bli?</label><br> <input type="text"
+					id="approximate_number_of_participants"
+					name="approximate_number_of_participants" required>
+			</div>
+						<div class="question">
+			<label for="housing_request">Hur vill ni bo som grupp?</label><br>
         
-        <?php 
+        <?php
 
-        	$sql = "SELECT * FROM HousingRequests;";
-        	$result = mysqli_query($conn, $sql);
-        	$resultCheck = mysqli_num_rows($result);
-        	
-        	if ($resultCheck > 0) {
-        		while ($row = mysqli_fetch_assoc($result)) {
-        		    echo "<input type='radio' id='housing_request" . $row['Id'] . "' name='housing_request' value='" . $row['Id'] . "'>";
-        		    echo "<label for='housing_request" .  $row['Id'] . "'>" . $row['Name'] . "</label><br>";
-        		}
-        	}
-        		
-	?> 
-        
-        <label for="need_fireplace">Behöver ni eldplats?:</label><br>
-		<input type="radio" id="html" name="need_fireplace" value="yes">
-  		<label for="html">Ja</label><br>
-  		<input type="radio" id="css" name="need_fireplace" value="no">
-  		<label for="css">Nej</label><br>
+        $sql = "SELECT * FROM HousingRequests;";
+        $result = mysqli_query($conn, $sql);
+        $resultCheck = mysqli_num_rows($result);
 
-        <label for="friends">Vänner:</label><br>
-		<textarea id="friends" name="friends" rows="4" cols="50">
-		</textarea><br>
-		 		
-        <label for="enemies">Fiender:</label><br>
-		<textarea id="enemies" name="enemies" rows="4" cols="50">
-		</textarea><br> 		
-  		
-	</p>
-      
-    </form>
-    </div>
-    </div>
-  </body>
+        if ($resultCheck > 0) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<input type='radio' id='housing_request" . $row['Id'] . "' name='housing_request' value='" . $row['Id'] . "'>";
+                echo "<label for='housing_request" . $row['Id'] . "'>" . utf8_decode($row['Name']) . "</label><br>";
+            }
+        }
+
+        ?> 
+        </div>
+        			<div class="question">
+				<label for="need_fireplace">Behöver ni eldplats?</label><br> <input
+					type="radio" id="html" name="need_fireplace" value="yes"> <label
+					for="html">Ja</label><br> <input type="radio" id="css"
+					name="need_fireplace" value="no"> <label for="css">Nej</label>
+			</div>
+			<div class="question">
+				<label for="friends">Vänner</label><br>
+				<textarea id="friends" name="friends" rows="4" cols="50">
+		</textarea>
+			</div>
+			<div class="question">
+				<label for="enemies">Fiender</label><br>
+				<textarea id="enemies" name="enemies" rows="4" cols="50">
+		</textarea>
+			</div>
+
+
+<div class="question">
+			Hur rik anser du att ni är? (1= äger inget, 5 = badar i pengar) 
+			
+			</div>
+			<div class="question">
+			Var
+			kommer grupen från? Dvs var är ni bosatta? 
+			</div>
+			<div class="question">
+			Vill ni ha en
+			arrangörsskriven gruppintrig inför lajvet? 
+			Om ni svarar "Nej" är det
+			inte en garanti för att ni inte får några intriger ändå. Speciellt om
+			ni redan är en existerande grupp i kampanjen så är det troligare att
+			ni är inblandade i något. 
+			</div>
+			
+			<div class="question">
+			
+			Vilka typer av gruppintriger är ni
+			intresserade av? Har ni några grupprykten som ni vill ha hjälp med
+			att sprida? (Om ja, specificera nedan) 
+			</div>
+						<div class="question">
+			Något annat arrangörerna bör
+			veta om er grupp? 
+</div>
+			<div class="question">
+			Genom att kryssa i denna ruta så lovar jag med
+			heder och samvete att jag har läst igenom alla hemsidans regler och
+			förmedlat dessa till mina gruppmedlemmar. Vi har även alla godkänt
+			dem och är införstådda med vad som förväntas av oss som grupp av
+			deltagare på lajvet. Om jag inte har läst reglerna så kryssar jag
+			inte i denna ruta. 
+			</div>
+						<div class="question">
+			Härmed samtycker jag till att föreningen Berghems
+			Vänner får spara och lagra mina uppgifter - såsom namn/
+			e-postadress/telefonnummer/hälsouppgifter/annat. Detta för att kunna
+			arrangera lajvet.
+</div>
+		</form>
+	</div>
+
+</body>
 </html>
