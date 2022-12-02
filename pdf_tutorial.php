167c1bec70f2278a2b6c0772c14d19d8aad46cbe
Mats Rappe <walderik@yahoo.se>
2022-12-02 16:06:01 +0100

    Test med bakgrundbild

--- pdf_tutorial.php
+++ pdf_tutorial.php
@@ -8,23 +8,24 @@ class TELEGRAM extends FPDF {
     
     function Header()
     {
-        global $title;
+//         global $title;
         
-        // Arial bold 15
-        $this->SetFont('Arial','B',15);
-        // Calculate width of title and position
-        $w = $this->GetStringWidth($title)+6;
-        $this->SetX((210-$w)/2);
-        // Colors of frame, background and text
-        $this->SetDrawColor(0,80,180);
-        $this->SetFillColor(230,230,0);
-        $this->SetTextColor(220,50,50);
-        // Thickness of frame (1 mm)
-        $this->SetLineWidth(1);
-        // Title
-        $this->Cell($w,9,$title,1,1,'C',true);
-        // Line break
-        $this->Ln(10);
+//         // Arial bold 15
+//         $this->SetFont('Arial','B',15);
+//         // Calculate width of title and position
+//         $w = $this->GetStringWidth($title)+6;
+//         $this->SetX((210-$w)/2);
+//         // Colors of frame, background and text
+//         $this->SetDrawColor(0,80,180);
+//         $this->SetFillColor(230,230,0);
+//         $this->SetTextColor(220,50,50);
+//         // Thickness of frame (1 mm)
+//         $this->SetLineWidth(1);
+//         // Title
+//         $this->Cell($w,9,$title,1,1,'C',true);
+//         // Line break
+//         $this->Ln(10);
+        $this->Image('telegram.png');
     }
     
     function SetText($text) {
