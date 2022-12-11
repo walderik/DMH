f4a1225184a0f79ade6843b3d394d5fc6969eaba
Mats Rappe <walderik@yahoo.se>
2022-12-11 13:56:11 +0100

    Create db_populator.php

---
+++ db_populator.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once 'includes/db.inc.php';
+
+# Bind - datatypes -   i	corresponding variable has type int
+//                     d	corresponding variable has type float
+//                     s	corresponding variable has type string
+//                     b	corresponding variable is a blob and will be sent in packets
+
+# Tabell experiences
+
+
+?>
