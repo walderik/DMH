b0905a8467b3215caec31f136e1b91d293914d53
Mats Rappe <walderik@yahoo.se>
2022-12-13 10:56:46 +0100

    Radera och editera telegram. Halvfärdigt.

--- telegram_admin.php
+++ telegram_admin.php
@@ -10,153 +10,161 @@ require 'telegram.php';
 <title>Administration av telegram</title>
 <link rel="stylesheet" href="includes/admin_system.css">
 
-      <script type = "text/javascript">  
-            function del(id) {
-            	var operation = document.getElementById("operation");
-        		var telegram_id = document.getElementById("Id");
-  				var submit_button = document.getElementById("submit_button");
-  
-               	var result = confirm("Är du säker på att du vill radera telegrammet? " + id);
-               	if (result == true) {
-            		operation.value = "delete";
-            		alert("Set " + operation.value);
+<script type="text/javascript">  
+    function del(id) {
+    	var operation = document.getElementById("operation");
+		var telegram_id = document.getElementById("Id");
+		var sender = document.getElementById("Sender");        		
+ 		var reciever = document.getElementById("Reciever");       		
+		var message = document.getElementById("Message");        		
+		
+		var submit_button = document.getElementById("submit_button");
 
-            		telegram_id.value = id;
-            		alert("Set " + telegram_id.value);
-            		submit_button.click();
-        		}
-            }  
+       	var result = confirm("Är du säker på att du vill radera telegrammet? " + id);
+       	if (result == true) {
+    		operation.value = "delete";
+    		telegram_id.value = id;
+    		sender.value = " ";
+    		reciever.value = " ";
+    		message.value = " ";
+    		submit_button.click();
+		}
+    }  
+    
+    
+    function fun(id); //, sendertxt, sendercitytxt, recievertxt, recievercitytxt, messagetxt) {
+    	alert("Edit");
+    	
+    	/*
+    	var operation = document.getElementById("operation");
+		var telegram_id = document.getElementById("Id");
+		var sender = document.getElementById("Sender");        		
+		var sendercity = document.getElementById("SenderCity");        		
+ 		var reciever = document.getElementById("Reciever");       		
+ 		var recievercity = document.getElementById("RecieverCity");       		
+		var message = document.getElementById("Message");        		
+		var notes = document.getElementById("OrganizerNotes");        		
+		
+		var submit_button = document.getElementById("submit_button");
+
+		operation.value = "update";
+		telegram_id.value = id;
+		sender.value = sendertxt;
+		sendercity.value = sendercitytxt;
+		reciever.value = recievertxt;
+		recievercity.value = recievercitytxt;
+		message.value = messagetxt.replace("<br>", "\n");
+		//notes.value = notestxt.replace("<br>", "\n");
+		submit_button.value = "Uppdatera";
+		*/
+    }  
+    
 
 </script>
- 
+
 </head>
-    <body>
+<body>
     
    
 <?php
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        
-        $operation = $_POST['operation'];
-        if ($operation == 'insert') {
-            echo "Insert";
-            $telegram = Telegram::newFromArray($_POST);
-            $telegram->create;
-        }
-        else if ($operation == 'delete'){
-            echo "Delete";
-        }
-        else if ($operation == 'update') {
-            echo "Update";
-        }
-        else {
-            echo $operation;
-        }
-    
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $operation = $_POST['operation'];
+    if ($operation == 'insert') {
+        echo "Insert";
+        $telegram = Telegram::newFromArray($_POST);
+        $telegram->create();
+    } else if ($operation == 'delete') {
+        echo "Delete " . $_POST['Id'];
+    } else if ($operation == 'update') {
+        echo "Update" . $_POST['Id'];
+    } else {
+        echo $operation;
     }
-    
-
-
+}
 
 ?>
     
     <h1>Telegram</h1>
     <?php
 
-//     $sql = "SELECT * FROM telegrams ORDER BY Deliverytime;";
-//     $result = mysqli_query($conn, $sql);
-//     $resultCheck = mysqli_num_rows($result);
-    
-//     if ($resultCheck > 0) {
-//         echo  "<table id='telegrams'>";
-//         echo "<tr><th>Id</td><th>Leveranstid</th><th>Avsändare</th><th>Avsändarens stad</th><th>Mottagare</th><th>Mottagarens stad</th><th>Meddelande</th><th>Anteckningar</th><th></th></tr>";
-//         while ($row = mysqli_fetch_assoc($result)) {
-//             echo  "<tr>";
-//             echo "<td>" . $row['Id'] . "</td>";
-//             echo "<td>" . $row['Deliverytime'] . "</td>";
-//             echo "<td>" . $row['Sender'] . "</td>";
-//             echo "<td>" . $row['SenderCity'] . "</td>";
-//             echo "<td>" . $row['Reciever'] . "</td>";
-//             echo "<td>" . $row['RecieverCity'] . "</td>";
-//             echo "<td>" . str_replace("\n", "<br>", $row['Message']) . "</td>";
-//             echo "<td>" . str_replace("\n", "<br>", $row['OrganizerNotes']) . "</td>";
-//             echo "<td>" . "<img src='images/remove-icon-hi.png' width='20' alt='Radera' onclick='confirm(Test);'></td>";
-//             //echo "<td>" . "<img src='images/remove-icon-hi.png' width='20' alt='Radera' onclick='delete_telegram(" . $row['Id'] .  ");'></td>";
-//             echo  "</tr>";
-//         }
-//         echo "</table>";
-//     }
-        $telegram_array = Telegram::all();
-        $resultCheck = count($telegram_array);
-        if ($resultCheck > 0) {
-            echo  "<table id='telegrams'>";
-            echo "<tr><th>Id</td><th>Leveranstid</th><th>Avsändare</th><th>Avsändarens stad</th><th>Mottagare</th><th>Mottagarens stad</th><th>Meddelande</th><th>Anteckningar</th><th></th></tr>";
-            foreach($telegram_array as $telegram) {
-                echo  "<tr>";
-                echo "<td>" . $telegram->id . "</td>";
-                echo "<td>" . $telegram->deliverytime . "</td>";
-                echo "<td>" . $telegram->sender . "</td>";
-                echo "<td>" . $telegram->senderCity . "</td>";
-                echo "<td>" . $telegram->reciever . "</td>";
-                echo "<td>" . $telegram->recieverCity . "</td>";
-                echo "<td>" . str_replace("\n", "<br>", $telegram->message) . "</td>";
-                echo "<td>" . str_replace("\n", "<br>", $telegram->organizerNotes) . "</td>";
-                echo "<td>" . "<img src='images/remove-icon-hi.png' width='20' alt='Radera' onclick='del(" . $telegram->id . ");' /></td>";
-                echo  "</tr>";
-            }
-            echo "</table>";
+    $telegram_array = Telegram::all();
+    $resultCheck = count($telegram_array);
+    if ($resultCheck > 0) {
+        echo "<table id='telegrams'>";
+        echo "<tr><th>Id</td><th>Leveranstid</th><th>Avsändare</th><th>Avsändarens stad</th><th>Mottagare</th><th>Mottagarens stad</th><th>Meddelande</th><th>Anteckningar</th><th></th><th></th></tr>\n";
+        foreach ($telegram_array as $telegram) {
+            echo "<tr>\n";
+            echo "<td>" . $telegram->id . "</td>\n";
+            echo "<td>" . $telegram->deliverytime . "</td>\n";
+            echo "<td>" . $telegram->sender . "</td>\n";
+            echo "<td>" . $telegram->senderCity . "</td>\n";
+            echo "<td>" . $telegram->reciever . "</td>\n";
+            echo "<td>" . $telegram->recieverCity . "</td>\n";
+            echo "<td>" . str_replace("\n", "<br>", $telegram->message) . "</td>\n";
+            echo "<td>" . str_replace("\n", "<br>", $telegram->organizerNotes) . "</td>\n";
+            
+            //"," . str_replace("\n", "<br>", $telegram->organizerNotes) . 
+            //echo "<td>" . "<img src='images/icons8-pencil-20.png' width='20' alt='Redigera' onclick='edit(" . $telegram->id . "," . $telegram->deliverytime . "," . $telegram->sender . "," . $telegram->senderCity . "," . $telegram->reciever . "," . $telegram->recieverCity . "," . str_replace("\n", "<br>", $telegram->message) . ");' /></td>\n";
+            //echo "<td>" . "<img src='images/icons8-pencil-20.png' width='20' alt='Redigera' onclick='edit(" . $telegram->id . "," . $telegram->deliverytime . "," . $telegram->sender . "," . $telegram->senderCity . "," . $telegram->reciever . "," . $telegram->recieverCity . "," . str_replace("\n", "<br>", $telegram->message) . ");' /></td>\n";
+            echo "<td>" . "<img src='images/icons8-pencil-20.png' width='20' alt='Redigera' onclick='fun(" . $telegram->id . ");' /></td>\n";
+            echo "<td>" . "<img src='images/icons8-trash-20.png' width='20' alt='Radera' onclick='del(" . $telegram->id . ");' /></td>\n";
+            echo "</tr>\n";
         }
-	?>
+        echo "</table>";
+    }
+    ?>
 	
 	<form method="post">
-	<input type="hidden" id="operation" name="operation" value="insert">
-	<input type="hidden" id="Id" name="telegram_id" value="">
-	<table>
-		<tr>
-			<td><label for="Deliverytime">Leveranstid</label></td>
-			<td><input type="datetime-local" id="Deliverytime"
-       name="Deliverytime" value="1868-09-13T17:00"
-       min="1868-09-13T17:00" max="1868-09-15T13:00" required></td>
-		</tr>
-		<tr>
-			<td><label for="Sender">Avsändare</label></td>
-			<td><input type="text" id="Sender" name="Sender" required></td>
-		</tr>
-		<tr>
+		<input type="hidden" id="operation" name="operation" value="insert"> <input
+			type="hidden" id="Id" name="Id" value="-1">
+		<table>
+			<tr>
+				<td><label for="Deliverytime">Leveranstid</label></td>
+				<td><input type="datetime-local" id="Deliverytime"
+					name="Deliverytime" value="1868-09-13T17:00" min="1868-09-13T17:00"
+					max="1868-09-15T13:00" required></td>
+			</tr>
+			<tr>
+				<td><label for="Sender">Avsändare</label></td>
+				<td><input type="text" id="Sender" name="Sender" required></td>
+			</tr>
+			<tr>
 
-			<td><label for="SenderCity">Avsändarens stad</label></td>
-			<td><input type="text" id="SenderCity" name="SenderCity" required></td>
-		</tr>
-		<tr>
+				<td><label for="SenderCity">Avsändarens stad</label></td>
+				<td><input type="text" id="SenderCity" name="SenderCity"
+					value="Junk City" required></td>
+			</tr>
+			<tr>
 
-			<td><label for="Reciever">Mottagare</label></td>
-			<td><input type="text" id="Reciever" name="Reciever" required></td>
-		</tr>
-		<tr>
+				<td><label for="Reciever">Mottagare</label></td>
+				<td><input type="text" id="Reciever" name="Reciever" required></td>
+			</tr>
+			<tr>
 
-			<td><label for="RecieverCity">Mottagarens stad</label></td>
-			<td><input type="text" id="RecieverCity" name="RecieverCity" value="Slow River" required></td>
-		</tr>
-		<tr>
+				<td><label for="RecieverCity">Mottagarens stad</label></td>
+				<td><input type="text" id="RecieverCity" name="RecieverCity"
+					value="Slow River" required></td>
+			</tr>
+			<tr>
 
-			<td><label for="Message">Meddelande</label></td>
-			<td>
-			<textarea id="Message" name="Message" rows="4" cols="50"></textarea>
-			</td>
-		</tr>
-		<tr>
+				<td><label for="Message">Meddelande</label></td>
+				<td><textarea id="Message" name="Message" rows="4" cols="50"
+						required></textarea></td>
+			</tr>
+			<tr>
 
-			<td><label for="OrganizerNotes">Anteckningar om telegrammet</label></td>
-			<td>
-			<textarea id="OrganizerNotes" name="OrganizerNotes" rows="4" cols="50"></textarea>
-			</td>
+				<td><label for="OrganizerNotes">Anteckningar om telegrammet</label></td>
+				<td><textarea id="OrganizerNotes" name="OrganizerNotes" rows="4"
+						cols="50"></textarea></td>
 
-		</tr>
+			</tr>
 
-	</table>
+		</table>
 
-				  <input id="submit_button" type="submit" value="Lägg till">
-		</form>
-	
-    </body>
+		<input id="submit_button" type="submit" value="Lägg till">
+	</form>
+
+</body>
 
 </html>
