84737c4009491216de8ecad0eeab8fd2122dc252
Mats Rappe <walderik@yahoo.se>
2023-01-15 12:22:35 +0100

    Ändrat textareas får rätt värden när det redan finns sparat.

--- participant/person_form.php
+++ participant/person_form.php
@@ -142,13 +142,13 @@ Det bör vara någon som inte är med på lajvet.</div>
 			<div class="question">
 				<label for="FoodAllergiesOther">Har du matallergier eller annan specialkost? </label><br>
 				<div class="explanation">Om du har allergier eller specialkost som inte täcks av de två ovanstående frågorna vill vi att du skriver om det här.</div>
-				<textarea id="FoodAllergiesOther" name="FoodAllergiesOther" value="<?php echo $person->FoodAllergiesOther; ?>" rows="4" cols="100"</textarea>
+				<textarea id="FoodAllergiesOther" name="FoodAllergiesOther" rows="4" cols="100"><?php echo $person->FoodAllergiesOther; ?></textarea>
 			</div>
 			
 			<div class="question">
 				<label for="OtherInformation">Övrig information</label><br>
 				<div class="explanation">Är det något annat kring din off-person arrangörerna bör veta? Tex andra allergier eller sjukdomar, eller bra kunskaper tex sjukvårdare.</div>
-				<textarea id="OtherInformation" name="OtherInformation" value="<?php echo $person->OtherInformation; ?>" rows="4" cols="100"></textarea>
+				<textarea id="OtherInformation" name="OtherInformation" rows="4" cols="100"><?php echo $person->OtherInformation; ?></textarea>
 			
 			 
 			</div>
