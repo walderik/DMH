fe1274de0432430a3da54db2be9377832d104794
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-01-22 17:24:11 +0100

    Uppdelning av admin

--- admin/index.php
+++ admin/index.php
@@ -7,6 +7,30 @@ include_once '../includes/error_handling.php';
         <nav id="navigation">
           <a href="#" class="logo"><?php echo $current_larp->Name;?></a>
           <ul class="links">
+              <li class="dropdown"><a href="#" class="trigger-drop">Admin<i class="arrow"></i></a>
+              <ul class="drop">
+                <li><a href="registrations.php">Deltagare</a></li>
+              </ul>
+            </li>
+              <li class="dropdown"><a href="#" class="trigger-drop">Intriger<i class="arrow"></i></a>
+              <ul class="drop">
+                <li><a href="roles.php">Karaktärer</a></li>
+                <li><a href="telegram_admin.php">Telegram</a></li>
+              </ul>
+            </li>
+              <li class="dropdown"><a href="#" class="trigger-drop">Köket<i class="arrow"></i></a>
+              <ul class="drop">
+                <li><a href="food.php">Matönskemål</a></li>
+                <li><a href="allergies.php">Allergier</a></li>
+              </ul>
+            </li>
+              <li class="dropdown"><a href="#" class="trigger-drop">Funktionärer<i class="arrow"></i></a>
+              <ul class="drop">
+                <li><a href="officials.php">Funktionärer</a></li>
+                <li><a href="npc.php">NPC'er</a></li>
+              </ul>
+            </li>
+        	<li><a href="super_admin.php" style="color: red"><i class="fa-solid fa-lock"></i>Super admin</a></li>  
             <li><a href="../participant/" style="color: #99bbff"><i class="fa-solid fa-unlock"></i>Deltagare</a></li>
         	<li><a href="../includes/logout.php"><i class="fa-solid fa-right-from-bracket"></i>Logga ut</a></li>
           </ul>
@@ -18,20 +42,8 @@ include_once '../includes/error_handling.php';
         	  <div class="error"><?php echo $error_message; ?></div>
         	  <div class="message"><?php echo $message_message; ?></div>
 			<p>
-			    <a href="telegram_admin.php">Telegram</a> <br> 
-			</p>
-			<h1>Administration för alla lajv</h1>
-			<p>			    
-			    <a href="larp_admin.php">Lajv</a> <br> 
-			    <a href="selection_data_admin.php?type=wealth">Rikedom</a><br>
-			    <a href="selection_data_admin.php?type=typesoffood">Matalternativ</a><br>
-			    <a href="selection_data_admin.php?type=placeofresidence">Var karaktärer/grupper bor</a><br>
-			    <a href="selection_data_admin.php?type=normalallergytypes">Vanliga allergier</a>	<br>		    			
-			    <a href="selection_data_admin.php?type=larpertypes">Typ av lajvare</a>	<br>		    			
-			    <a href="selection_data_admin.php?type=officialtypes">Typ av funktionärer</a>	<br>		    			
-			    <a href="selection_data_admin.php?type=intriguetypes">Typ av intriger</a>	<br>		    			 			
-			    <a href="selection_data_admin.php?type=housingrequests">Boendeönskemål</a>	<br>		    			
-			    <a href="selection_data_admin.php?type=experiences">Erfarenhet som lajvare</a>	<br>		    			
+				Just nu är det XX anmälda deltagare.
+			     <br> 
 			</p>
 		</div>
 	</body>
