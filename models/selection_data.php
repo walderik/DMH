b423ae0d2cb20674a02a2b63b905890eedd480f8
Mats Rappe <walderik@yahoo.se>
2023-01-22 17:50:19 +0100

    Används existerande helper

--- models/selection_data.php
+++ models/selection_data.php
@@ -88,33 +88,8 @@ class SelectionData extends BaseModel{
     # En dropdown där man kan välja den här
     public static function selectionDropdown(?bool $multiple=false, ?bool $required=true, $selected=null) {
         $selectionDatas = static::allActive();
-        $name = ($multiple) ? (static::class . "Id[]") : static::class."Id";
-
-        # TODO Hantera required för checkboxes när det behövs - Det går med Javascripts
-        //     https://tutorialdeep.com/knowhow/make-checkbox-field-required-form-html/
-        //     https://stackoverflow.com/questions/11787665/making-sure-at-least-one-checkbox-is-checked
-        //     Men enklast är nog att göra en kontroll när man sparar formuläret och ger ett felmeddelande om värdet saknas.
-        $option = ($required) ? ' required' : '';
-        $type = ($multiple) ? "checkbox" : "radio";
-        
-        echo "<div class='selectionDropdown'>\n";
-        foreach ($selectionDatas as $selectionData) { 
-            $row_option = $option;
-            # Kolla om något är selected
-            if(!$multiple) {
-                if (!is_null($selected) && $selected == $selectionData->Id) 
-                    $row_option = $row_option.' checked="checked"';
-            } else {
-                if (!is_null($selected) && !empty($selected) && in_array($selectionData->Id, $selected))
-                    $row_option = $row_option.' checked="checked"';
-            }
-            
-            echo "<input type='" . $type . "' id='" . $selectionData->Id . "' name='" . $name . "' value='" . $selectionData->Id . "' " . $row_option . ">\n";
-            echo "<label for='" . $selectionData->Id . "'>" .  $selectionData->Name . "</label><br>\n";
-        }
-        echo "</div>\n";
-        
-    
+//         $name = ($multiple) ? (static::class . "Id[]") : static::class."Id";
+        selectionDropdownByArray(static::class , $selectionDatas, $multiple, $required, $selected);
     }
 
     
