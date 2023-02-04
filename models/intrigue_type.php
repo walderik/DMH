990733775caa029cd49fecac02f2f5ce1ee430fa
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-02-04 11:50:57 +0100

    Bugg i sql

--- models/intrigue_type.php
+++ models/intrigue_type.php
@@ -5,7 +5,7 @@ class IntrigueType extends SelectionData{
 
     public static function getIntrigeTypesForLarpAndRole($larpId, $roleId) {
         if (is_null($larpId) || is_null($roleId)) return array();
-        $sql = "SELECT * from `intriguetype` WHERE Id in (SELECT * FROM `intriguetype_larp_role` WHERE LARP_RoleLARPid = ? AND LARP_RoleRoleId = ?)  ORDER BY ORDER BY SortOrder;";
+        $sql = "SELECT * from `intriguetype` WHERE Id in (SELECT IntrigueTypeId FROM `intriguetype_larp_role` WHERE LARP_RoleLARPid = ? AND LARP_RoleRoleId = ?)  ORDER BY SortOrder;";
         $stmt = static::connectStatic()->prepare($sql);
         
         if (!$stmt->execute(array($larpId, $roleId))) {
@@ -32,7 +32,7 @@ class IntrigueType extends SelectionData{
     public static function getIntrigeTypesForLarpAndGroup($larpId, $groupId) {
         if (is_null($larpId) || is_null($groupId)) return array();
         
-        $sql = "SELECT * from `intriguetype` WHERE Id in (SELECT * FROM `intriguetype_larp_group` WHERE LARP_GroupGroupId = ? AND LARP_GroupLARPId = ?) ORDER BY ORDER BY SortOrder;";
+        $sql = "SELECT * from `intriguetype` WHERE Id in (SELECT IntrigueTypeId FROM `intriguetype_larp_group` WHERE LARP_GroupGroupId = ? AND LARP_GroupLARPId = ?) ORDER BY SortOrder;";
         $stmt = static::connectStatic()->prepare($sql);
         
         if (!$stmt->execute(array($groupId, $larpId))) {
