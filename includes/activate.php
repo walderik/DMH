0d69cafd8825b5a47b28f1b83afe1b1524a5b3d5
Mats Rappe <walderik@yahoo.se>
2023-02-26 11:17:49 +0100

    Fix så aktivering inte ger felmeddelande om det går bra

--- includes/activate.php
+++ includes/activate.php
@@ -6,13 +6,14 @@ include_once $root . '/includes/all_includes.php';
 // First we check if the email and code exists...
 if (isset($_GET['email'], $_GET['code'])) {
     $user = User::loadByEmail($_GET['email']);
-    if (!isset($user) or $user->ActivationCode != $_GET['code']) {
+    if (!isset($user) || $user->ActivationCode != $_GET['code']) {
         header('Location: ../index.php?error=activation_not_possible');
         exit;
     }
     $user->ActivationCode = 'activated';
     $user->update();
     header('Location: ../index.php?message=activated');
+    exit;
 }
 
 header('Location: ../index.php?error=activation_not_possible');
