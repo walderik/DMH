1644f62f951bed6559d2e2365c69b092c8925f2a
Mats Rappe <walderik@yahoo.se>
2023-02-26 16:15:58 +0100

    Fix så fält dä rman kunnat mata in radbrytningar visas med radbrytningarna

--- participant/view_role.php
+++ participant/view_role.php
@@ -43,18 +43,19 @@ if (isset($current_role->GroupId)) {
 		<table>
 			<tr><td valign="top" class="header">Spelas av</td><td><?php echo $current_role->getPerson()->Name; ?></td></tr>
 		<?php if (isset($group)) {?>
-			<tr><td valign="top" class="header">Grupp</td><td><a href ="view_group.php?id=<?php echo $group->Id;?>"><?php echo $group->Name;?></a></td></tr>
+			<tr><td valign="top" class="header">Grupp</td><td><a href ="view_group.php?id=<?php echo $group->Id;?>"><?php echo $group->Name; ?></a></td></tr>
 		<?php }?>
 			<tr><td valign="top" class="header">Huvudkaraktär</td><td><?php echo ja_nej($larp_role->IsMainRole);?></td></tr>
 			<tr><td valign="top" class="header">NPC</td><td><?php echo ja_nej($current_role->IsNPC);?></td></tr>
 			<tr><td valign="top" class="header">Yrke</td><td><?php echo $current_role->Profession;?></td></tr>
+			<tr><td valign="top" class="header">Beskrivning</td><td><?php echo nl2br($current_role->Description);?></td></tr>
 			<tr><td valign="top" class="header">Tidigare lajv</td><td><?php echo $current_role->PreviousLarps;?></td></tr>
 			<tr><td valign="top" class="header">Varför befinner sig karaktären i Slow River?</td><td><?php echo $current_role->ReasonForBeingInSlowRiver;?></td></tr>
 			<tr><td valign="top" class="header">Religion</td><td><?php echo $current_role->Religion;?></td></tr>
 			<tr><td valign="top" class="header">Mörk hemlighet</td><td><?php echo $current_role->DarkSecret;?></td></tr>
-			<tr><td valign="top" class="header">Mörk hemlighet - intrig idéer</td><td><?php echo $current_role->DarkSecretIntrigueIdeas;?></td></tr>
+			<tr><td valign="top" class="header">Mörk hemlighet - intrig idéer</td><td><?php echo nl2br($current_role->DarkSecretIntrigueIdeas); ?></td></tr>
 			<tr><td valign="top" class="header">Intrigtyper</td><td><?php echo commaStringFromArrayObject($larp_role->getIntrigueTypes());?></td></tr>
-			<tr><td valign="top" class="header">Intrigidéer</td><td><?php echo $current_role->IntrigueSuggestions;?></td></tr>
+			<tr><td valign="top" class="header">Intrigidéer</td><td><?php echo nl2br($current_role->IntrigueSuggestions); ?></td></tr>
 			<tr><td valign="top" class="header">Saker karaktären inte vill spela på</td><td><?php echo $current_role->NotAcceptableIntrigues;?></td></tr>
 			<tr><td valign="top" class="header">Relationer med andra</td><td><?php echo $current_role->CharactersWithRelations;?></td></tr>
 			<tr><td valign="top" class="header">Annan information</td><td><?php echo $current_role->OtherInformation;?></td></tr>
