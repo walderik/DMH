e91251b4b441acb2130d0d0a948087604a6bf7a6
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-02-28 16:59:40 +0100

    Lagt till betalat och tagit bort anmäld på listan över deltagare

--- admin/registered_persons.php
+++ admin/registered_persons.php
@@ -20,7 +20,7 @@
     		    echo "Inga anmälda deltagare";
     		} else {
     		    echo "<table class='data'>";
-    		    echo "<tr><th>Namn</th><th>Epost</th><th>Ålder på lajvet</th><th>Mobilnummer</th><th></th><th>Anmäld</th><th>Godkänd</th><th>Medlem</th></tr>\n";
+    		    echo "<tr><th>Namn</th><th>Epost</th><th>Ålder på lajvet</th><th>Mobilnummer</th><th></th><th>Godkänd</th><th>Betalat</th><th>Medlem</th></tr>\n";
     		    foreach ($persons as $person)  {
     		        echo "<tr>\n";
     		        echo "<td>" . $person->Name . "</td>\n";
@@ -29,22 +29,10 @@
     		        echo "<td>" . $person->PhoneNumber . "</td>\n";
     		        
     		        echo "<td>" . "<a href='person_form.php?operation=update&id=" . $person->Id . "'><i class='fa-solid fa-pen'></i></td>\n";
-    		        echo "<td align='center'>" . showStatusIcon($person->isRegistered($current_larp)) . "</td>\n";
     		        echo "<td align='center'>" . showStatusIcon($person->isApproved($current_larp)) . "</td>\n";
+    		        echo "<td align='center'>" . showStatusIcon($person->hasPayed($current_larp)) . "</td>\n";
     		        echo "<td align='center'>" . showStatusIcon($person->isMember($current_larp->StartDate)) . "</td>\n";
     		        echo "</tr>\n";
-    		        $roles = $person->getRoles();
-    		        foreach ($roles as $role)  {
-    		            echo "<tr>\n";
-    		            echo "<td></td>\n";
-    		            echo "<td>" . $role->Name . "</td>\n";
-    		            echo "<td>" . $role->Profession . "</td>\n";
-    		            
-    		            echo "<td>" . "<a href='show_role.php?id=" . $role->Id . "'><i class='fa-solid fa-eye'></i></td>\n";
-    		            echo "<td>" . "<a href='../participant/role_form.php?operation=update&id=" . $role->Id . "'><i class='fa-solid fa-pen'></i></td>\n";
-    		            echo "</tr>\n";
-    		            
-    		        }
     		    }
     		    echo "</table>";
     		}
