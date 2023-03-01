2599e4fe83be3268e85dff06991bc81ed448ea83
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-03-01 19:06:15 +0100

    Update persons_to_approve.php

--- admin/persons_to_approve.php
+++ admin/persons_to_approve.php
@@ -14,35 +14,40 @@ include_once 'header.php';
         </nav>
 
     <div class="content">   
-        <h1>Anmälda deltagare</h1>
+        <h1>Deltagare med karaktärer som ska godkännas</h1>
      		<?php 
     		$persons = Person::getAllToApprove($current_larp);
     		if (empty($persons)) {
     		    echo "Alla anmälda är godkända";
     		} else {
     		    echo "<table class='data'>";
-    		    echo "<tr><th>Namn</th><th>Epost</th><th>Ålder på lajvet</th><th>Mobilnummer</th><th></th><th>Godkänd</th><th>Medlem</th><th colspan='2'>Betalat</th></tr>\n";
+    		    echo "<tr><th>Namn</th><th>Ålder</th><th>Epost</th><th>Mobilnummer</th><th></th><th>Godkänn</th></tr>\n";
     		    foreach ($persons as $person)  {
     		        echo "<tr>\n";
     		        echo "<td>" . $person->Name . "</td>\n";
-    		        echo "<td>" . $person->Email . "</td>\n";
     		        echo "<td>" . $person->getAgeAtLarp($current_larp) . " år</td>\n";
+    		        echo "<td>" . $person->Email . "</td>\n";
     		        echo "<td>" . $person->PhoneNumber . "</td>\n";
     		        
-    		        echo "<td>" . "<a href='approval_form.php?id=" . $person->Id . "'><i class='fa-solid fa-eye'></i></a></td>\n";
+    		        echo "<td>" . "<a href='view_person.php?id=" . $person->Id . "'><i class='fa-solid fa-eye'></i></a></td>\n";
     		        echo "</tr>\n";
     		        $roles = $person->getRolesAtLarp($current_larp);
     		        foreach($roles as $role) {
     		            echo "<tr>\n";
-    		            echo "<td>" . $role->Name . "</td>\n";
+    		            echo "<td>&nbsp;&nbsp;&nbsp;" . $role->Name . "</td>\n";
     		            echo "<td>" . $role->Profession . "</td>\n";
     		            echo "<td>" . $role->getGroup()->Name . "</td>\n";
+    		            echo "<td>";
     		            if (LARP_Role::loadByIds($role->Id, $current_larp->Id)->IsMainRole == 1) {
-    		              echo "<td>Huvudkaraktär</td>\n";
+    		              echo "Huvudkaraktär";
     		            }
+    		            echo "</td>\n";
+    		            echo "<td>" . "<a href='view_role.php?id=" . $person->Id . "'><i class='fa-solid fa-eye'></i></a></td>\n";
+    		            
     		            echo "</tr>\n";
     		            
     		        }
+    		        echo "<tr></tr>";
     		    }
     		    echo "</table>";
     		}
