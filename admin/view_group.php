593f2f1ae0ba17584146b8c2cbe48ba810a6570a
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-03-03 09:49:49 +0100

    Länkar för visa grupp och roll

--- admin/view_group.php
+++ admin/view_group.php
@@ -36,7 +36,7 @@ $group_members = Role::getRegisteredRolesInGroup($current_group, $current_larp);
 	<div class="content">
 		<h1><?php echo $current_group->Name;?></h1>
 		<table>
-			<tr><td valign="top" class="header">Gruppansvarig</td><td><?php echo Person::loadById($current_group->PersonId)->Name;?></td></tr>
+			<tr><td valign="top" class="header">Gruppansvarig</td><td><a href ="view_person.php?id=<?php echo $current_group->PersonId;?>"><?php echo $current_group->getPerson()->Name;?></a></td></tr>
 			<tr><td valign="top" class="header">Beskrivning</td><td><?php echo $current_group->Description;?></td></tr>
 			<tr><td valign="top" class="header">Vänner</td><td><?php echo $current_group->Friends;?></td></tr>
 			<tr><td valign="top" class="header">Fiender</td><td><?php echo $current_group->Enemies;?></td></tr>
@@ -59,9 +59,11 @@ $group_members = Role::getRegisteredRolesInGroup($current_group, $current_larp);
 
 		foreach($group_members as $group_member) {
 
-		    echo $group_member->Name . " - " . 
-                 $group_member->Profession . " spelas av " . 
-                 $group_member->getPerson()->Name;
+		    echo "<a href ='view_role.php?id=" . $group_member->Id ."'>" . 
+		    $group_member->Name . "</a> - " . 
+            $group_member->Profession . " spelas av " . 
+            "<a href ='view_person.php?id=" . $group_member->getPerson()->Id . "'>" .
+            $group_member->getPerson()->Name . "</a>";
 
 
             if ($group_member->getPerson()->getAgeAtLarp($current_larp) < $current_larp->getCampaign()->MinimumAgeWithoutGuardian) {
