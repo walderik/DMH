cb239c39fb837d5500b8d81fb52f57f251e133e2
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-03-03 17:06:52 +0100

    Mail när man blir godkänd

--- admin/logic/approve_person.php
+++ admin/logic/approve_person.php
@@ -21,9 +21,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $registration->Approved = date("Y-m-d");
         
         $registration->update();
+        send_approval_mail($registration);
         header('Location: ../persons_to_approve.php');
         exit;
     }
     
 }
 header('Location: ../index.php');
+
+
+
+function send_approval_mail(Registration $registration) {
+    $person = $registration->getPerson();
+    $mail = $person->Email;
+    
+    $larp = $registration->getLARP();
+    $roles = $person->getRolesAtLarp($larp);
+    
+    $campaign = $larp->getCampaign();
+    
+    $text  = "Dina karaktärer är nu godkända för att vara med i lajvet $larp->Name<br>\n";
+    $text .= "<br>\n";
+    $text .= "De karaktärer du har anmält är:<br>\n";
+    $text .= "<br>\n";
+    foreach ($roles as $role) {
+        $text .= '* '.$role->Name;
+        if ($role->isMain($larp)) {
+            $text .= " - Din huvudkaraktär";
+        } elseif ($role->IsNPC) {
+            $text .= " - En NPC";
+        }
+        $text .= "<br>\n";
+    }
+    
+    DmhMailer::send($mail, $person->Name, $text, "Bekräftan av anmälan till ".$larp->Name);
+}
