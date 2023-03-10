ab29070f2a0dd7fa7ca0d3a562ef6aa9dd604b51
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-03-10 17:45:56 +0100

    Tagit bort kod som aldrig används

--- participant/logic/group_registration_form_save.php
+++ participant/logic/group_registration_form_save.php
@@ -13,23 +13,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $larp_group = LARP_Group::newFromArray($_POST);
         $larp_group->create();
         $larp_group->saveAllIntrigueTypes($_POST);
-    } elseif ($operation == 'update') {
-        //TODO Kolla om man är gruppledare annars får man inte ändra på gruppen
-        
-        
-        $larp_group = LARP_Group::newFromArray($_POST);
-        
-        $group = Group::loadById($larp_group->GroupId);
-        //Kolla om man är gruppledare annars får man inte ändra på gruppen
-        if (!$current_user->isGroupLeader($group)) {
-            header('Location: ../index.php');
-            exit;
-        }
-        
-        $larp_group->update();
-        $larp_group->deleteAllIntrigueTypes();
-        $larp_group->saveAllIntrigueTypes($_POST);
-        
     } else {
         echo $operation;
     }
