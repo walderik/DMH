d197ac3a122d41e1d1ce3d9d895533ec9f9a9df5
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-03-11 12:03:54 +0100

    Lagt till betalningsreferens

--- admin/registered_persons.php
+++ admin/registered_persons.php
@@ -13,7 +13,7 @@
     		    echo "Inga anmälda deltagare";
     		} else {
     		    echo "<table class='data'>";
-    		    echo "<tr><th>Namn</th><th>Epost</th><th>Ålder på lajvet</th><th>Mobilnummer</th><th></th><th>Godkänd</th><th>Medlem</th><th colspan='2'>Betalat</th></tr>\n";
+    		    echo "<tr><th>Namn</th><th>Epost</th><th>Ålder på lajvet</th><th>Mobilnummer</th><th></th><th>Godkänd</th><th>Medlem</th><th>Betalnings-<br>referens</th><th colspan='2'>Betalat</th></tr>\n";
     		    foreach ($persons as $person)  {
     		        echo "<tr>\n";
     		        echo "<td>" . $person->Name . "</td>\n";
@@ -25,6 +25,7 @@
     		        echo "<a href='edit_person.php?id=" . $person->Id . "'><i class='fa-solid fa-pen'></i></a></td>\n";
     		        echo "<td align='center'>" . showStatusIcon($person->isApproved($current_larp)) . "</td>\n";
     		        echo "<td align='center'>" . showStatusIcon($person->isMember($current_larp)) . "</td>\n";
+    		        echo "<td>".$person->getRegistration($current_larp)->PaymentReference .  "</td>\n";
     		        echo "<td align='center'>" . showStatusIcon($person->hasPayed($current_larp)) . "</td><td><a href='person_payment.php?id=" . $person->Id . "'><i class='fa-solid fa-pen'></i></a></td>\n";
     		        echo "</tr>\n";
     		    }
