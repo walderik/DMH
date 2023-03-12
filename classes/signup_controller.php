75254f2f2949f4636892afc8ff04e179aba25bcc
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-03-12 14:04:11 +0100

    Inloggning

--- classes/signup_controller.php
+++ classes/signup_controller.php
@@ -1,13 +1,17 @@
 <?php
+global $root;
+$root = $_SERVER['DOCUMENT_ROOT'] . "/regsys";
 
+require $root . '/includes/init.php';
 
-class SignupController extends Signup{
+class SignupController {
     private $name;
     private $email;
     private $password;
     private $passwordrepeat;
     
     public function __construct($name, $email, $password, $passwordrepeat) {
+        echo "Contruct";
         $this->name = $name;
         $this->email = $email;
         $this->password = $password;
@@ -15,6 +19,7 @@ class SignupController extends Signup{
     }
     
     public function signupUser() {
+        echo "Sinupuser";
         if ($this->emptyInput()) {
             header("location: ../index.php?error=emptyInput");
             exit();
@@ -93,5 +98,47 @@ class SignupController extends Signup{
         }
         return $result;
     }
+    
+    private function createUser($name, $email, $password) {
+        $user = User::newWithDefault();
+        $user->Name = $name;
+        $user->Email = $email;
+        
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $activationCode = uniqid();
+        
+        $user->Password = $hashedPassword;
+        $user->ActivationCode = $activationCode;
+        
+        $user->create();
+        
+        $url = $this->activation_url($email, $activationCode);
+        
+        $text  = "Du har registrerat ett konto för anmälningssystemet.<br>\n";
+        $text .= "Nu måste du aktivera ditt konto.<br><br>\n";
+        $text .= "<a href='$url'>Allt du behöver göra är att klicka på den här länken.</a><br>\n";
+        
+        BerghemMailer::send($email, $name, $text, "Aktiveringsbrev");
+        
+    }
+    
+    
+    private function checkUserExists($email) {
+        $user = User::loadByEmail($email);
+        
+        if (is_null($user)) return false;
+        
+        return true;
+    }
+    
+    private function activation_url($email, $activationCode){
+        return sprintf(
+            "%s://%s%s",
+            isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+            $_SERVER['SERVER_NAME'],
+            "/regsys/includes/activate.php?email=$email&code=$activationCode"
+            );
+    }
+    
     
 }
