21ce435a847e36a563e2babc2531e1b8a1e19d4f
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-03-15 14:34:50 +0100

    Förklarande text

--- admin/doh_ssn_check.php
+++ admin/doh_ssn_check.php
@@ -9,6 +9,7 @@ include 'navigation_subpage.php';
 	<div class="content">
 		<h1>DOH 2023 medlemskontroll</h1>
 				<form action="doh_ssn_check_result.php" method="post">
+				<p>Skriv ett personnumer per rad.</p>
 				<textarea id="ssn_list" name="ssn_list" rows="20" cols="20" maxlength="60000" required></textarea><br>
 				<input type="submit" value="Kontrollera">
 				
