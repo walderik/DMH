7690d29d68d345c410d90806e282e2b6c8ca02f0
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-03-17 16:57:43 +0100

    Ny header

--- index.php
+++ index.php
@@ -10,14 +10,25 @@ include_once 'includes/error_handling.php';
 <!DOCTYPE html>
 <html>
 	<head>
+	<style>
+	
+	.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+
+}
+	
+	</style>
 		<meta charset="utf-8">
-		<title>Berghems vänners anmälningssystem</title>
+		<title>Berghems vänners Omnes Mundos</title>
 		<link href="css/loginpage.css" rel="stylesheet" type="text/css">
 		<link rel="icon" type="image/x-icon" href="../images/bv.ico">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 	<body>
-	  <h1>Berghems vänners anmälningssystem</h1>
+	<img  class="center" src="images/omnes_mundos_header.jpg">
+
 	  <?php if (isset($error_message) && strlen($error_message)>0) {
 	      echo '<div class="error">'.$error_message.'</div>';
 	  }?>
