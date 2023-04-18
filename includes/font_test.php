156c7774fd637698e6acb23698fe7a41bedd81cf
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-04-18 14:22:31 +0200

    Fonter

--- includes/font_test.php
+++ includes/font_test.php
@@ -2,5 +2,8 @@
 global $root;
 $root = $_SERVER['DOCUMENT_ROOT'] . "/regsys";
 require_once $root . '/pdf/our_fonts_pdf.php';
+//print_r(OurFonts::fontArray());
+//print_r(OurFonts::fontsToLoad());
+
 our_fonts_pdf::print_test();
 
