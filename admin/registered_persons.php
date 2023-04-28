484952a88db34049a24cb15305e920b379c5a739
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-04-28 13:52:59 +0200

    Sortering

--- admin/registered_persons.php
+++ admin/registered_persons.php
@@ -3,7 +3,12 @@
  
  include 'navigation_subpage.php';
 ?>
+<style>
+th {
+  cursor: pointer;
+}
 
+</style>
 
     <div class="content">   
         <h1>Anmälda deltagare</h1>
@@ -12,9 +17,16 @@
     		if (empty($persons)) {
     		    echo "Inga anmälda deltagare";
     		} else {
-    		    echo "<table class='data'>";
-    		    echo "<tr><th>Namn</th><th></th><th>Epost</th><th>Ålder<br>på lajvet</th><th>Godkända<br>karaktärer</th>";
-    		    echo "<th>Medlem</th><th>Betalnings-<br>referens</th><th colspan='2'>Betalat</th><th colspan='2'>Plats på lajvet</th></tr>\n";
+    		    echo "<table id='participants' class='data'>";
+    		    echo "<tr><th onclick='sortTable(0)'>Namn</th>".
+    		      "<th></th>".
+    		      "<th onclick='sortTable(2)'>Epost</th>".
+    		      "<th onclick='sortTable(3)'>Ålder<br>på lajvet</th>".
+    		      "<th onclick='sortTable(4)'>Godkända<br>karaktärer</th>".
+    		      "<th onclick='sortTable(5)'>Medlem</th>".
+    		      "<th onclick='sortTable(6)'>Betalnings-<br>referens</th>".
+    		      "<th onclick='sortTable(7)' colspan='2'>Betalat</th>".
+    		      "<th onclick='sortTable(9)' colspan='2'>Plats på lajvet</th></tr>\n";
     		    foreach ($persons as $person)  {
     		        $registration = $person->getRegistration($current_larp);
     		        echo "<tr>\n";
@@ -69,5 +81,63 @@
         
 	</div>
 </body>
+
+
+<script>
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("participants");
+  switching = true;
+  // Set the sorting direction to ascending:
+  dir = "asc";
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // Start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /* Loop through all table rows (except the
+    first, which contains table headers): */
+    for (i = 1; i < (rows.length - 1); i++) {
+      // Start by saying there should be no switching:
+      shouldSwitch = false;
+      /* Get the two elements you want to compare,
+      one from current row and one from the next: */
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /* Check if the two rows should switch place,
+      based on the direction, asc or desc: */
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          // If so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark that a switch has been done: */
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      // Each time a switch is done, increase this count by 1:
+      switchcount ++;
+    } else {
+      /* If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again. */
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+</script>
 
 </html>
