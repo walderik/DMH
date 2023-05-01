9831e42f4f5ad821bc7c4e5c4ac72b48caba0beb
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-05-01 13:39:58 +0200

    Generisk tabell-sortering

--- admin/registered_persons.php
+++ admin/registered_persons.php
@@ -3,30 +3,27 @@
  
  include 'navigation_subpage.php';
 ?>
-<style>
-th {
-  cursor: pointer;
-}
 
-</style>
 
     <div class="content">   
         <h1>Anmälda deltagare</h1>
+        Genom att klicka på rubrikerna i tabellen kan du sortera tabellen. Klicka en gång till för att få omvänd ordning.
      		<?php 
     		$persons = Person::getAllRegistered($current_larp);
     		if (empty($persons)) {
     		    echo "Inga anmälda deltagare";
     		} else {
-    		    echo "<table id='participants' class='data'>";
-    		    echo "<tr><th onclick='sortTable(0)'>Namn</th>".
+    		    $tableId = "participants";
+    		    echo "<table id='$tableId' class='data'>";
+    		    echo "<tr><th onclick='sortTable(0, \"$tableId\");'>Namn</th>".
     		      "<th></th>".
-    		      "<th onclick='sortTable(2)'>Epost</th>".
-    		      "<th onclick='sortTable(3)'>Ålder<br>på lajvet</th>".
-    		      "<th onclick='sortTable(4)'>Godkända<br>karaktärer</th>".
-    		      "<th onclick='sortTable(5)'>Medlem</th>".
-    		      "<th onclick='sortTable(6)'>Betalnings-<br>referens</th>".
-    		      "<th onclick='sortTable(7)' colspan='2'>Betalat</th>".
-    		      "<th onclick='sortTable(9)' colspan='2'>Plats på lajvet</th></tr>\n";
+    		      "<th onclick='sortTable(2, \"$tableId\")'>Epost</th>".
+    		      "<th onclick='sortTable(3, \"$tableId\")'>Ålder<br>på lajvet</th>".
+    		      "<th onclick='sortTable(4, \"$tableId\")'>Godkända<br>karaktärer</th>".
+    		      "<th onclick='sortTable(5, \"$tableId\")'>Medlem</th>".
+    		      "<th onclick='sortTable(6, \"$tableId\")'>Betalnings-<br>referens</th>".
+    		      "<th onclick='sortTable(7, \"$tableId\")' colspan='2'>Betalat</th>".
+    		      "<th onclick='sortTable(9, \"$tableId\")' colspan='2'>Plats på lajvet</th></tr>\n";
     		    foreach ($persons as $person)  {
     		        $registration = $person->getRegistration($current_larp);
     		        echo "<tr>\n";
@@ -82,62 +79,5 @@ th {
 	</div>
 </body>
 
-
-<script>
-function sortTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("participants");
-  switching = true;
-  // Set the sorting direction to ascending:
-  dir = "asc";
-  /* Make a loop that will continue until
-  no switching has been done: */
-  while (switching) {
-    // Start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
-      shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount ++;
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
-</script>
-
+<?php include_once '../javascript/table_sort.js';?>
 </html>
