dd5a7e9af580342f921a6e328a586490df997580
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-05-29 18:27:34 +0200

    Lagfart producerar / behöver

--- admin/titledeed_admin.php
+++ admin/titledeed_admin.php
@@ -1,6 +1,7 @@
 <?php
 include_once 'header.php';
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $operation = $_POST['operation'];
@@ -8,21 +9,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($operation == 'insert') {
         $titledeed = Titledeed::newFromArray($_POST);
         $titledeed->create();
+        if (isset($_POST['ProducesId'])) {
+            $titledeed->setProduces($_POST['ProducesId']);
+        }
+        if (isset($_POST['RequiresId'])) {
+            $titledeed->setRequires($_POST['RequiresId']);
+        }
     } elseif ($operation == 'delete') {
         Titledeed::delete($_POST['Id']);
     } elseif ($operation == 'update') {
         $titledeed = Titledeed::loadById($_POST['Id']);
         $titledeed->setValuesByArray($_POST);
         $titledeed->update();
-    } 
+        $titledeed->deleteAllProduces();
+        $titledeed->deleteAllRequires();
+        if (isset($_POST['ProducesId'])) {
+            $titledeed->setProduces($_POST['ProducesId']);
+        }
+        if (isset($_POST['RequiresId'])) {
+            $titledeed->setRequires($_POST['RequiresId']);
+        }
+    } elseif ($operation == 'add_titledeed_owner_role') {
+        $titledeed = Titledeed::loadById($_POST['id']);
+        $titledeed->addOwner($_POST['RoleId']);       
+    }
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     
-//     $operation = $_GET['operation'];
-    if (isset($_GET['operation']) && $_GET['operation'] == 'delete') {
-        Titledeed::delete($_GET['id']);
+    if (isset($_GET['operation'])) {
+        $operation = $_GET['operation'];
+        if ($operation == 'delete') {
+            Titledeed::delete($_GET['id']);
+        }
+        elseif ($operation == 'delete_owner') {
+            $titledeeId=$_GET['titledeeId'];
+            $roleId=$_GET['roleId'];
+            $titledeed = Titledeed::loadById($titledeeId);
+            $titledeed->deleteOwner($roleId);
+        }
     }
+ 
 }
 
 include 'navigation.php';
@@ -53,7 +80,22 @@ include 'navigation.php';
                 echo "<td>" . ja_nej($titledeed->IsTradingPost) . "</td>\n";
                 echo "<td>" . commaStringFromArrayObject($titledeed->Produces()) . "</td>\n";
                 echo "<td>" . commaStringFromArrayObject($titledeed->Requires()) . "</td>\n";
-                echo "<td>" . $owner . "</td>\n";
+                echo "<td>";
+                
+                $owner_roles = $titledeed->owners();
+                foreach ($owner_roles as $owner_role) {
+                    echo "$owner_role->Name <a href='titledeed_admin.php?operation=delete_owner&titledeeId=$titledeed->Id&roleId=$owner_role->Id'><i class='fa-solid fa-trash'></i></a><br>";
+                    
+                }
+                
+                echo "<form action='choose_role.php' method='post'>";
+                echo "<input type='hidden' id='operation' name='operation' value='add_titledeed_owner_role'>";
+                echo "<input type='hidden' id='id' name='id' value='$titledeed->Id'>";
+                echo "<input id='submit_button' type='submit' value='Lägg till en karaktär som ägare'>";
+                echo "</form>";
+
+                
+                echo "</td>\n";
                 
                 echo "<td>" . "<a href='titledeed_form.php?operation=update&id=" . $titledeed->Id . "'><i class='fa-solid fa-pen'></i></td>\n";
                 echo "<td>" . "<a href='titledeed_admin.php?operation=delete&id=" . $titledeed->Id . "'><i class='fa-solid fa-trash'></i></td>\n";
