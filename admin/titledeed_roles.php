baa3548e0c99b7886abdf118fcf6dbdbadb3c6bf
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-07-04 14:19:16 +0200

    Lista på alla som håller på med handel

--- admin/titledeed_roles.php
+++ admin/titledeed_roles.php
@@ -6,50 +6,49 @@
 
 
     <div class="content">   
-        <h1>Karaktärer</h1>
-        <h2>Huvudkaraktärer</h2>
+        <h1>Karaktärer med handel</h1>
+        <p>Sidokaraktärer markeras med en * efter namnet</p>
      		<?php 
-     		$roles = $current_larp->getAllMainRoles(true);
+     		$roles = $current_larp->getAllRoles();
     		if (empty($roles)) {
     		    echo "Inga anmälda karaktärer";
     		} else {
     		    $tableId = "main_roles";
     		    echo "<table id='$tableId' class='data'>";
     		    echo "<tr><th onclick='sortTable(0, \"$tableId\");'>Namn</th>".
-        		    "<th onclick='sortTable(1, \"$tableId\")'>Plats<br>på lajvet</th>".
+        		    "<th onclick='sortTable(1, \"$tableId\")'>Kommer<br>på lajvet</th>".
         		    "<th onclick='sortTable(2, \"$tableId\")'>Rikedom</th>".
         		    "<th onclick='sortTable(3, \"$tableId\")'>Grupp</th>".
         		    "<th onclick='sortTable(4, \"$tableId\")'>Lagfarter</th>".
                     "</tr>\n";
     		    foreach ($roles as $role)  {
-    		        if ($role->is_trading($current_larp)) {
-        		        $person = $role->getPerson();
-        		        $registration=$person->getRegistration($current_larp);
+    		        //TODO reda ut logiken
+    		        //Man vill se alla roller som kommer på lajvet och har handel och 
+    		        //alla roller som äger lagfart oavsett om de kommer eller inte
+    		        $titledeeds = Titledeed::getAllForRole($role);
+    		        $larp_role = LARP_Role::loadByIds($role->Id, $current_larp->Id);
+    		        $person = $role->getPerson();
+    		        $registration=$person->getRegistration($current_larp);
+    		        $isComing = !empty($larp_role);
+    		        if (!empty($registration) && $registration->isNotComing()) $isComing=false;
+    		        if (($isComing && $role->is_trading($current_larp)) || !empty($titledeeds)) {
         		        echo "<tr>\n";
         		        echo "<td>";
-        		        if ($registration->isNotComing()) {
-        		            echo "<s>";
-        		            echo $role->Name;
-        		            echo "</s>";
-        		            echo "</td>";
-        		        }
-        		        else {
-        		            echo "<a href='view_role.php?id=$role->Id'>$role->Name</a>\n";
-            		        if ($role->IsDead ==1) echo " <i class='fa-solid fa-skull-crossbones' title='Död'></i>";
-            		        echo "</td>\n";
-            		        echo "<td align='center'>".showStatusIcon($person->getRegistration($current_larp)->hasSpotAtLarp())."</td>\n";
-            		        $wealth = $role->getWealth();
-            		        echo "<td>";
-            		        if (!empty($wealth)) echo $wealth->Name;
-           		           $group = $role->getGroup();
-            		        if (is_null($group)) {
-            		            echo "<td>&nbsp;</td>\n";
-            		        } else {
-            		            echo "<td><a href='view_group.php?id=$group->Id'>$group->Name</a></td>\n";
-            		        }
+    		            echo "<a href='view_role.php?id=$role->Id'>$role->Name</a>\n";
+                        if (!empty($larp_role) && ($larp_role->IsMainRole == 0)) echo " * ";
+    		            if ($role->IsDead ==1) echo " <i class='fa-solid fa-skull-crossbones' title='Död'></i>";
+        		        echo "</td>\n";
+        		        echo "<td align='center'>".showStatusIcon($isComing)."</td>\n";
+        		        $wealth = $role->getWealth();
+        		        echo "<td>";
+        		        if (!empty($wealth)) echo $wealth->Name;
+       		           $group = $role->getGroup();
+        		        if (is_null($group)) {
+        		            echo "<td>&nbsp;</td>\n";
+        		        } else {
+        		            echo "<td><a href='view_group.php?id=$group->Id'>$group->Name</a></td>\n";
         		        }
         		        echo "<td>";
-        		        $titledeeds = Titledeed::getAllForRole($role);
         		        
         		        foreach ($titledeeds as $titledeed) {
         		            echo "$titledeed->Name<br>";
@@ -63,12 +62,7 @@
     		
     		?>
 
-        
-                <h2>Övriga karaktärer</h2>
-                <p>Antal ord på intrig och vilka intriger de är med i visas inte för nedanstående karaktärer.</p>
-        
-	</div>
-</body>
+ </body>
 <?php 
 include_once '../javascript/table_sort.js';
 ?>
