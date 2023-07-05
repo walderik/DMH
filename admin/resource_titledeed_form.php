34217956127332d926feecd3cf40528bc48ef336
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-07-05 10:07:02 +0200

    Direkt uppdatering av resultat

--- admin/resource_titledeed_form.php
+++ admin/resource_titledeed_form.php
@@ -26,10 +26,44 @@ input[type="number"] {
   text-align: right;
 }
 </style>
+
+
+<script>
+
+function updateResult() {
+
+	var result = 0;
+    result = result + document.getElementById("Produces_Money").value;
+    result = result - document.getElementById("Requires_Money").value;
+
+
+	const producesArr = document.getElementsByClassName("Produces");
+	const requiresArr = document.getElementsByClassName("Requires");
+
+
+    for (let i = 0; i < producesArr.length; i++) {
+      var cost = document.getElementById(producesArr[i].name + "Cost").value;
+      result = result + producesArr[i].value * cost;
+    }
+
+
+    for (let i = 0; i < requiresArr.length; i++) {
+      var cost = document.getElementById(requiresArr[i].name + "Cost").value;
+      result = result - requiresArr[i].value * cost;
+    }
+
+
+    var currency = document.getElementById("Currency").value; 
+	document.getElementById("result").innerHTML = result + " " + currency;
+
+}
+
+</script>
     <div class="content"> 
     <h1>Redigera resurser för <?php echo $titledeed->Name ?></h1>
 	<form action="logic/resource_titledeed_form_save.php" method="post">
 		<input type="hidden" id="Id" name="Id" value="<?php echo $titledeed->Id ?>">
+		<input type="hidden" id="Currency" value="<?php echo $currency ?>">
 		<table>
 		<tr><td colspan="2"><h2>Normal drift</h2><p>En lagfart kan antingen producera eller behöva en resurs. 
     Om man skriver in att den både producerar och behöver kommer skillnaden att räknas ut.</p></td>
@@ -39,7 +73,7 @@ input[type="number"] {
 				<?php 
 				$money = 0;
 				if ($titledeed->Money > 0) $money = abs($titledeed->Money);
-				echo "<input type='number' name='Produces_Money' value = '$money' min='0' required> ";
+				echo "<input type='number' id='Produces_Money' name='Produces_Money' value = '$money' min='0' onchange='updateResult()' required> ";
 				echo $currency;
 				echo "<br>";
 				
@@ -47,7 +81,8 @@ input[type="number"] {
 				    $resource_titledeed = Resource_Titledeed::loadByIds($resource->Id, $titledeed->Id);
 				    $quantity = 0;
 				    if (!empty($resource_titledeed) && $resource_titledeed->Quantity > 0) $quantity = abs($resource_titledeed->Quantity);
-				    echo "<input type ='number' name='Produces_$resource->Id' value = '$quantity' min='0' required> ";
+				    echo "<input class='Produces' type ='number' name='Produces_$resource->Id' value = '$quantity' min='0' onchange='updateResult()' required> ";
+                    echo "<input type='hidden' id='Produces_".$resource->Id."Cost' value='$resource->Price'>";
 				    if (in_array($resource->Id, $normally_produces_resourceIds)) {
 				        echo "<span style='color:green;font-weight: bold;'>$resource->Name</span>";
 				    }
@@ -72,7 +107,7 @@ input[type="number"] {
 				<?php 
 				$money = 0;
 				if ($titledeed->Money < 0) $money = abs($titledeed->Money);
-				echo "<input type ='number' name='Requires_Money' value = '$money' min='0' required> ";
+				echo "<input type ='number' id='Requires_Money' name='Requires_Money' value = '$money' min='0' onchange='updateResult()' required> ";
 				echo $currency;
 				echo "<br>";
 				
@@ -80,7 +115,8 @@ input[type="number"] {
 				    $resource_titledeed = Resource_Titledeed::loadByIds($resource->Id, $titledeed->Id,);
 				    $quantity = 0;
 				    if (!empty($resource_titledeed) && $resource_titledeed->Quantity < 0) $quantity = abs($resource_titledeed->Quantity);
-				    echo "<input type ='number' name='Requires_$resource->Id' value = '$quantity' min='0' required> ";
+				    echo "<input type ='number' class='Requires' name='Requires_$resource->Id' value = '$quantity' min='0' onchange='updateResult()' required> ";
+				    echo "<input type='hidden' id='Requires_".$resource->Id."Cost' value='$resource->Price'>";
 				    if (in_array($resource->Id, $normally_requires_resourceIds)) {
 				        echo "<span style='color:green;font-weight: bold;'>$resource->Name</span>";
 				    }
