28f9299e3bc5b246a2c108f1849fab473a6de0d8
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-07-07 10:57:02 +0200

    Visa mail lite tydligare

--- admin/mail_admin.php
+++ admin/mail_admin.php
@@ -12,9 +12,8 @@ include 'navigation.php';
         echo "<tr><th onclick='sortTable(0, \"$tableId\");'>Till</th>".
 	    "<th onclick='sortTable(1, \"$tableId\")'>Ämne</th>".
 	    "<th onclick='sortTable(2, \"$tableId\")'>Skickat av</th>".
-	    "<th onclick='sortTable(3, \"$tableId\")'>Skapat</th>".
 	    "<th onclick='sortTable(4, \"$tableId\")'>Skickat</th>".
-	    "<th onclick='sortTable(5, \"$tableId\")'>Felmeddelande</th>".
+	    "<th onclick='sortTable(5, \"$tableId\")'>Fel</th>".
         "<th></th>".
         "</tr>\n";
     	
@@ -25,9 +24,12 @@ include 'navigation.php';
     	    echo "<td>$email->ToName ($email->To)</td>";
     	    echo "<td>$email->Subject</td>";
     	    echo "<td>$user->Name</td>";
-    	    echo "<td>$email->CreatedAt</td>";
     	    echo "<td>$email->SentAt</td>";
-    	    echo "<td>$email->ErrorMessage</td>";
+    	    echo "<td>";
+    	    if (!empty($email->ErrorMessage)) {
+    	        echo showStatusIcon(false);
+    	    }
+    	    echo "</td>";
     	    echo "<td><a href='view_email.php?id=$email->Id'><i class='fa-solid fa-eye'></i></a>\n";
     	    //echo " <a href='mail_admin.php?operation=delete&id=" . $email->Id . "'><i class='fa-solid fa-trash'></i></td>\n";
     	    echo "</tr>";
