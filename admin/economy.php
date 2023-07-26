ff39858d5e66d4a06b23860f8fc652f4e902e508
Mats Rappe <walderik@yahoo.se>
2023-07-26 12:42:00 +0200

    Bättre radbryt

--- admin/economy.php
+++ admin/economy.php
@@ -55,9 +55,9 @@ include 'navigation.php';
         En varning betyder att det saknas ett kvitto på en utgift.<br><br>
         Konton läggs upp under <a href="settings.php">inställningar</a></p>
         <p>
-        <a href="economy_add_income.php"><i class="fa-solid fa-file-circle-plus"></i> Lägg till inkomst</a>
-        <a href="economy_add_expense.php"><i class="fa-solid fa-file-circle-plus"></i> Lägg till utgift</a>
-        <a href="reports/economy.php" target="_blank"><i class="fa-solid fa-file-pdf"></i> Generera rapport till kassör</a><br>
+        <a href="economy_add_income.php"><i class="fa-solid fa-file-circle-plus"></i> Lägg till inkomst</a> &nbsp; 
+        <a href="economy_add_expense.php"><i class="fa-solid fa-file-circle-plus"></i> Lägg till utgift</a><br>
+        <a href="reports/economy.php" target="_blank"><i class="fa-solid fa-file-pdf"></i> Generera rapport till kassör</a> &nbsp; 
         <a href="logic/all_bookkeeping_pdf.php" target="_blank"><i class="fa-solid fa-file-pdf"></i> Alla verifikationer till kassör</a><br>
         </p>
 	<?php 
