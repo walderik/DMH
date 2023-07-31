de37328c306bafbf17fd224920675e78bd664947
Karin Rappe <walderik@yahoo.se>
2023-07-31 18:54:26 +0200

    Välja lajv

--- admin/navigation.php
+++ admin/navigation.php
@@ -27,7 +27,7 @@ function myFunction() {
     <div id="left">
     	<a href="#home" target="_blank" style="padding: 11px 5px 11px 5px;" class="always_show">
     	<img src="../images/<?php echo $current_larp->getCampaign()->Icon; ?>" width="30" height="30"/></a>
-    	<a href="choose_larp.php" id="larp_name" class="logo always_show"><?php echo $current_larp->Name;?></a>
+    	<a href="../participant/choose_larp.php" id="larp_name" class="logo always_show"><?php echo $current_larp->Name;?></a>
     </div>
     <div id="right">
 
--- site-admin/navigation.php
+++ site-admin/navigation.php
@@ -27,7 +27,7 @@ function myFunction() {
     <div id="left">
     	<a href="#home" target="_blank" style="padding: 11px 5px 11px 5px;" class="always_show">
     	<img src="../images/bv.ico" width="30" height="30"/></a>
-    	<a href="choose_larp.php" id="larp_name" class="logo always_show">Omnes Mundos</a>
+    	<a href="../participant/choose_larp.php" id="larp_name" class="logo always_show">Omnes Mundos</a>
     </div>
     <div id="right">
 
