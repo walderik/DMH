934d3ca6c321d1af8c21ebd610ce386bf1f46c33
Karin Rappe <walderik@yahoo.se>
2023-08-10 08:45:59 +0200

    Snyggare rekvisita översikt

--- admin/prop_admin.php
+++ admin/prop_admin.php
@@ -26,7 +26,7 @@ include 'navigation.php';
        $prop_array = Prop::allByCampaign($current_larp);
         if (!empty($prop_array)) {
             echo "<table class='data'>";
-            echo "<tr><th>Id</td><th>Namn</th><th>Beskrivning</th><th>Lagerplats</th><th>Märkning</th><th>In-lajv<br>egenskaper</th><th>Innehavare</th><th>Bild</th><th>Intrig</th><th></th><th></th></tr>\n";
+            echo "<tr><th>Id</td><th>Namn</th><th>Beskrivning</th><th>Lagerplats</th><th>Märkning</th><th>In-lajv<br>egenskaper</th><th>Innehavare</th><th>Bild</th><th>Används<br>i intrig</th><th></th><th></th></tr>\n";
             foreach ($prop_array as $prop) {
                 $owner = "";
                 if (isset($prop->GroupId)) {
@@ -48,7 +48,7 @@ include 'navigation.php';
 
                 if ($prop->hasImage()) {
                     $image = Image::loadById($prop->ImageId);
-                    echo "<td><img width=30 src='data:image/jpeg;base64,".base64_encode($image->file_data)."'/> <a href='logic/delete_image.php?id=$prop->Id&type=prop'>Ta bort bild</a></td>\n";
+                    echo "<td><img width=30 src='data:image/jpeg;base64,".base64_encode($image->file_data)."'/> <a href='logic/delete_image.php?id=$prop->Id&type=prop'><i class='fa-solid fa-trash' title='Ta bort bild'></i></a></td>\n";
                 }
                 else {
                     echo "<td><a href='upload_image.php?id=$prop->Id&type=prop'><i class='fa-solid fa-image-portrait' title='Ladda upp bild'></i></a></td>\n";
