0fdda26f3479c9648b98c14431dc407ae67b2300
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-08-23 20:09:28 +0200

    Align top

--- admin/resource_titledeed_overview_normal.php
+++ admin/resource_titledeed_overview_normal.php
@@ -76,7 +76,7 @@ th {
 		
 		foreach ($titledeeds as $titledeed) {
 		    echo "<tr><td style='text-align:left'><a href='titledeed_form.php?operation=update&id=$titledeed->Id'>$titledeed->Name</a></td>";
-		    echo "<th style='text-align:left'>";
+		    echo "<th valign='top' style='text-align:left'>";
 		    echo "<input type='number' id='$titledeed->Id' value='$titledeed->Money' onchange='recalculateMoney(this, $current_larp->Id)'>";
 		    echo "</th>\n";
 		    $produces = $titledeed->ProducesNormally();
