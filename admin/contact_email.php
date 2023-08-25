ccd5b7a0cb95518a9f8de05f6342073071728686
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-08-25 08:14:58 +0200

    Bättre feltext

--- admin/contact_email.php
+++ admin/contact_email.php
@@ -78,7 +78,7 @@ include 'navigation.php';
 		} elseif (isset($_POST['send_intrigues'])) {
 		    echo "<h1>Skicka ut intrigerna till alla deltagarna.</h1>\n";
 		    echo "I mailet kommer karaktärsbladen till alla deras karaktärer och grupper som de är med i bifogas.<br>\n";
-		    echo "Det kommer ta några minuter att skicka till alla.<br>Det går iväg som mest ett mail var 15 sekund till max 15 mottagare.<br>\n";
+		    echo "Det kommer ta några minuter att skicka till alla.<br>Det går iväg som mest ett mail var 15 sekund.<br>\n";
 		    echo "För att mailen ska gå iväg måste du fortsätta att använda systemet.<br>\n";
 		} elseif (isset($_POST['send_housing'])) {
 		    echo "<h1>Skicka ut boendet till alla deltagarna.</h1>\n";
