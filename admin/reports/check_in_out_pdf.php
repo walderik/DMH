0125ac0f00f3ee43b65e6a46c7215668c9558517
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-08-28 12:59:40 +0200

    Visa incheckning för grupper

--- admin/reports/check_in_out_pdf.php
+++ admin/reports/check_in_out_pdf.php
@@ -32,16 +32,37 @@ function cmp($a, $b)
     return ($a->Name < $b->Name) ? -1 : 1;
 }
 
-//Vem bor var?
 $persons = Person::getAllRegistered($current_larp, false);
 usort($persons, "cmp");
 $rows = array();
-$rows[] = array("Namn", "Incheck", "Utcheck", "Kommentar");
+$rows[] = array("Namn", "Incheck", "Utcheck", "Kommentar                              ");
 foreach ($persons as $person) {
-    $rows[] = array($person->Name, "", "", "                              ");
+    $props = Prop::getCheckinProps($person, $current_larp);
+    //$letters = Letter::
+    $props_txt_Arr = array();
+    foreach($props as $prop) $props_txt_Arr[] = $prop->Name;
+    $comment = "";
+    if (!empty($props_txt_Arr)) $comment = "Ska ha vid incheck: ". implode(", ", $props_txt_Arr);
+    $rows[] = array($person->Name, "", "", $comment);
 }
-$pdf->new_report($current_larp, "In- och utcheckning", $rows);
+$pdf->new_report($current_larp, "In- och utcheckning deltagare", $rows);
     
-    
+
+
+$groups = Group::getAllRegistered($current_larp);
+$rows = array();
+$rows[] = array("Namn", "Incheck", "Utcheck", "Kommentar                              ");
+foreach ($groups as $group) {
+    $props = Prop::getCheckinPropsForGroup($group, $current_larp);
+    //$letters = Letter::
+    $props_txt_Arr = array();
+    foreach($props as $prop) $props_txt_Arr[] = $prop->Name;
+    $comment = "";
+    if (!empty($props_txt_Arr)) $comment = "Ska ha vid incheck: ". implode(", ", $props_txt_Arr);
+    $rows[] = array($group->Name, "", "", $comment);
+}
+$pdf->new_report($current_larp, "In- och utcheckning grupper", $rows);
+
+
     
 $pdf->Output();
