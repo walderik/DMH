d30aedc59bad03f4de57fd5f98941c71dcd7beaa
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-09-01 12:42:14 +0200

    Rapport med anhöriga

--- admin/reports.php
+++ admin/reports.php
@@ -41,6 +41,7 @@ include 'navigation.php';
     <h3>Övrigt</h3>
 		<a href="reports/check_in_out_pdf.php" target="_blank"><i class="fa-solid fa-file-pdf"></i>In och utcheckning</a><br>
 		<a href="reports/medical.php" target="_blank"><i class="fa-solid fa-file-pdf"></i>Hälsoinformation till sjukvårdare/trygghetsvärdar</a><br>
+		<a href="reports/emergency_contacts.php" target="_blank"><i class="fa-solid fa-file-pdf"></i>Anhöriga</a><br>
     </div>
     <h2>Övrigt</h2>
     <p>
---
+++ admin/reports/emergency_contacts.php
@@ -0,0 +1,46 @@
+<?php
+# Läs mer på http://www.fpdf.org/
+
+global $root, $current_user, $current_larp;
+$root = $_SERVER['DOCUMENT_ROOT'] . "/regsys";
+
+require_once $root . '/pdf/report_pdf.php';
+
+include_once '../header.php';
+
+if ($_SERVER["REQUEST_METHOD"] != "GET") {
+    header('Location: ../../admin/index.php');
+    exit;
+}
+
+$name = 'Närmaste anhörig';
+
+$pdf = new Report_PDF();
+
+$pdf->SetTitle(utf8_decode($name));
+$pdf->SetAuthor(utf8_decode($current_user->Name));
+$pdf->SetCreator('Omnes Mundos');
+$pdf->AddFont('Helvetica','');
+$pdf->SetSubject(utf8_decode($name));
+
+
+function cmp($a, $b)
+{
+    if ($a->Name == $b->Name) {
+        return 0;
+    }
+    return ($a->Name < $b->Name) ? -1 : 1;
+}
+
+$persons = Person::getAllRegistered($current_larp, false);
+usort($persons, "cmp");
+$rows = array();
+$rows[] = array("Namn", "Kontaktperson                              ");
+foreach ($persons as $person) {
+    $rows[] = array($person->Name, $person->EmergencyContact);
+}
+$pdf->new_report($current_larp, $name, $rows);
+    
+
+    
+$pdf->Output();
