15a8d9c81b4d38e1e325fbc557a76e39b7d434ec
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-09-13 17:12:49 +0200

    Decimaler i rapporten också

--- admin/reports/economy.php
+++ admin/reports/economy.php
@@ -30,7 +30,7 @@ $rows[] = array('Verifikation','Datum','Rubrik','Konto', 'Summa');
 $sum = 0;
 foreach($bookkeepings as $bookkeeping) {
     $rows[] = array($bookkeeping->Number, $bookkeeping->Date, 
-        $bookkeeping->Headline, $bookkeeping->getBookkeepingAccount()->Name, $bookkeeping->Amount);
+        $bookkeeping->Headline, $bookkeeping->getBookkeepingAccount()->Name, number_format((float)$bookkeeping->Amount, 2, ',', ''));
     $sum += $bookkeeping->Amount;
     
 
@@ -38,13 +38,13 @@ foreach($bookkeepings as $bookkeeping) {
 $registration_fees = Registration::totalFeesPayed($current_larp);
 $sum += $registration_fees;
 
-$rows[] = array('', substr($current_larp->EndDate,0,10), 'Deltagaravgifter', '', $registration_fees);
+$rows[] = array('', substr($current_larp->EndDate,0,10), 'Deltagaravgifter', '', number_format((float)$registration_fees, 2, ',', ''));
 
 $returned_fees = Registration::totalFeesReturned($current_larp);
 $sum -= $returned_fees;
-$rows[] = array('', substr($current_larp->EndDate,0,10), 'Återbetalade deltagaravgifter', '', ' '.(0 - $returned_fees));
+$rows[] = array('', substr($current_larp->EndDate,0,10), 'Återbetalade deltagaravgifter', '', ' '.number_format((float)(0-$returned_fees), 2, ',', ''));
 
-$rows[] = array('', '', 'Summa', '', $sum);
+$rows[] = array('', '', 'Summa', '', number_format((float)$sum, 2, ',', ''));
 
 $pdf->new_report($current_larp, $name, $rows);
 
