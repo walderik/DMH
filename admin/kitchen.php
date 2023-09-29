b208ccd47683eb1e808f2175c58d144f91157288
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2023-09-29 12:41:49 +0200

    Visa matalternativ för köket, bättre

--- admin/kitchen.php
+++ admin/kitchen.php
@@ -11,9 +11,10 @@ include 'navigation.php';
     <h2>Vald mat</h2>
     <?php 
     $foodChoises = Registration::getFoodVariants($current_larp);
-    
+    $hasFoodChoices = false;
     echo "<table class='smalldata'>";
     foreach($foodChoises as $foodChoise) {
+        if (!empty($foodChoise[0])) $hasFoodChoices = true;
         echo "<tr><td>".$foodChoise[0] . "</td><td>" . $foodChoise[1] . "</td><td>" . $foodChoise[2] . " st</td></tr>"; 
     }
     echo "</table>";
@@ -32,12 +33,14 @@ include 'navigation.php';
             $persons = Person::getAllWithSingleAllergy($allergy, $current_larp);
             if (isset($persons) && count($persons) > 0) {
                 echo "<h3>Enbart $allergy->Name</h3><table class='data'>";
-                echo "<tr><th>Namn</th><th>Epost</th><th>Telefon</th><th>Övrigt</th><th>Vald mat</th><th>Matalternativ</th></tr>";
+                echo "<tr><th>Namn</th><th>Epost</th><th>Telefon</th><th>Övrigt</th><th>Vald mat</th>";
+                if ($hasFoodChoices) echo "<th>Matalternativ</th>";
+                echo "</tr>";
                 foreach($persons as $person) {
                     $registration=$person->getRegistration($current_larp);
                     echo "<tr><td>$person->Name</td><td>$person->Email ".contactEmailIcon($person->Name,$person->Email)."</td>";
                     echo "<td>$person->PhoneNumber</td><td>$person->FoodAllergiesOther</td><td>".$registration->getTypeOfFood()->Name."</td>";
-                    echo "<td>$registration->FoodChoice</td>";
+                    if ($hasFoodChoices) echo "<td>$registration->FoodChoice</td>";
                     echo "</tr>";
                 }
                 echo "</table>";
@@ -50,13 +53,15 @@ include 'navigation.php';
         if (!empty($persons) && count($persons) > 0) {
             echo "<h3>Multipla vanliga allergier</h3>";
             echo "<table class='data'>";
-            echo "<tr><th>Namn</th><th>Epost</th><th>Telefon</th><th>Allergier</th><th>Övrigt</th><th>Vald mat</th><th>Matalternativ</th></tr>";
+            echo "<tr><th>Namn</th><th>Epost</th><th>Telefon</th><th>Allergier</th><th>Övrigt</th><th>Vald mat</th>";
+            if ($hasFoodChoices) echo "<th>Matalternativ</th>";
+            echo "</tr>";
             foreach($persons as $person) {
                 $registration=$person->getRegistration($current_larp);
                 echo "<tr><td>$person->Name</td><td>$person->Email ".contactEmailIcon($person->Name,$person->Email)."</td>";
                 echo "<td>$person->PhoneNumber</td><td>" . commaStringFromArrayObject($person->getNormalAllergyTypes()) . "</td>";
                 echo "<td>$person->FoodAllergiesOther</td><td>" . $registration->getTypeOfFood()->Name . "</td>";
-                echo "<td>$registration->FoodChoice</td>";
+                if ($hasFoodChoices) echo "<td>$registration->FoodChoice</td>";
                 echo "</tr>";
             }
             echo "</table>";
@@ -64,17 +69,19 @@ include 'navigation.php';
     }
     
     //Hitta alla som inte har någon vald allergi, men som har en kommentar
-    //TODO borde sorteras per matalternativ
+    //TODO borde kanske sorteras per matalternativ
     
     $persons = Person::getAllWithoutAllergiesButWithComment($current_larp);
     if (!empty($persons) && count($persons) > 0) {
         echo "<h3>Special</h3><table class='data'>";
-        echo "<tr><th>Namn</th><th>Epost</th><th>Telefon</th><th>Övrigt</th><th>Vald mat</th><th>Matalternativ</th></tr>";
+        echo "<tr><th>Namn</th><th>Epost</th><th>Telefon</th><th>Övrigt</th><th>Vald mat</th>";
+        if ($hasFoodChoices) echo "<th>Matalternativ</th>";
+        echo "</tr>";
         foreach($persons as $person) {
             $registration=$person->getRegistration($current_larp);
             echo "<tr><td>$person->Name</td><td>$person->Email ".contactEmailIcon($person->Name,$person->Email)."</td>";
             echo "<td>$person->PhoneNumber</td><td>$person->FoodAllergiesOther</td><td>" . $registration->getTypeOfFood()->Name . "</td>";
-            echo "<td>$registration->FoodChoice</td>";
+            if ($hasFoodChoices) echo "<td>$registration->FoodChoice</td>";
             echo "</tr>";
         }
         echo "</table>";
--- admin/reports/matlista.php
+++ admin/reports/matlista.php
@@ -15,6 +15,17 @@ if ($_SERVER["REQUEST_METHOD"] != "GET") {
 
 $name = 'Matlista';
 
+
+$foodChoises = Registration::getFoodVariants($current_larp);
+$hasFoodChoices = false;
+foreach($foodChoises as $foodChoise) {
+    if (!empty($foodChoise[0])) {
+        $hasFoodChoices = true;
+        break;
+    }
+}
+
+
 $pdf = new Report_PDF();
 
 $pdf->SetTitle(utf8_decode($name));
@@ -30,11 +41,14 @@ if (NormalAllergyType::isInUse()){
         $persons = Person::getAllWithSingleAllergy($allergy, $current_larp);
         if (isset($persons) && count($persons) > 0) {
             $rows = array();
-            $rows[] = array('Namn','Epost','Övrigt','Vald Mat');
+            if ($hasFoodChoices) $rows[] = array('Namn','Epost','Övrigt','Vald Mat', 'Matalternativ');
+            else $rows[] = array('Namn','Epost','Övrigt','Vald Mat');
     
             foreach($persons as $person) {
                 $registration=$person->getRegistration($current_larp);
-                $rows[] = array($person->Name, $person->Email, 
+                if ($hasFoodChoices) $rows[] = array($person->Name, $person->Email,
+                    $person->FoodAllergiesOther, $registration->getTypeOfFood()->Name, $registration->FoodChoice);
+                else $rows[] = array($person->Name, $person->Email, 
                                 $person->FoodAllergiesOther, $registration->getTypeOfFood()->Name);
             }
     
@@ -46,10 +60,14 @@ if (NormalAllergyType::isInUse()){
     $persons = Person::getAllWithMultipleAllergies($current_larp);
     if (!empty($persons) && count($persons) > 0) {
         $rows = array();
-        $rows[] = array('Namn','Epost','Allergier','Övrigt','Vald Mat');
+        if ($hasFoodChoices) $rows[] = array('Namn','Epost','Allergier','Övrigt','Vald Mat', 'Matalternativ');
+        else $rows[] = array('Namn','Epost','Allergier','Övrigt','Vald Mat');
+        
         foreach($persons as $person) {
             $registration=$person->getRegistration($current_larp);
-            $rows[] = array($person->Name, $person->Email, commaStringFromArrayObject($person->getNormalAllergyTypes()),
+            if ($hasFoodChoices) $rows[] = array($person->Name, $person->Email, commaStringFromArrayObject($person->getNormalAllergyTypes()),
+                $person->FoodAllergiesOther, $registration->getTypeOfFood()->Name, $registration->FoodChoice);
+            else $rows[] = array($person->Name, $person->Email, commaStringFromArrayObject($person->getNormalAllergyTypes()),
                             $person->FoodAllergiesOther, $registration->getTypeOfFood()->Name);
         }
         $pdf->new_report($current_larp, "Multipla vanliga allergier", $rows);
@@ -60,10 +78,15 @@ if (NormalAllergyType::isInUse()){
 $persons = Person::getAllWithoutAllergiesButWithComment($current_larp);
 if (!empty($persons) && count($persons) > 0) {
     $rows = array();
-    $rows[] = array('Namn','Epost','Övrigt','Vald Mat');
+    if ($hasFoodChoices) $rows[] = array('Namn','Epost','Övrigt','Vald Mat','Matalternativ');
+    else $rows[] = array('Namn','Epost','Övrigt','Vald Mat');
+    
     foreach($persons as $person) {
         $registration=$person->getRegistration($current_larp);
-        $rows[] = array($person->Name, $person->Email, 
+        
+        if ($hasFoodChoices) $rows[] = array($person->Name, $person->Email,
+            $person->FoodAllergiesOther, $registration->getTypeOfFood()->Name, $registration->FoodChoice);
+        else $rows[] = array($person->Name, $person->Email, 
                         $person->FoodAllergiesOther, $registration->getTypeOfFood()->Name);
     }
     $pdf->new_report($current_larp, "Special", $rows);
