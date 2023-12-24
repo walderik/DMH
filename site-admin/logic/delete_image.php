7c5280fdca0cb55c4958bbaca330440a55499031
Mats Rappe <walderik@yahoo.se>
2023-12-24 11:45:32 +0100

    Missad logik för radera bild

---
+++ site-admin/logic/delete_image.php
@@ -0,0 +1,48 @@
+<?php
+include_once '../header.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    
+    if (isset($_GET['id']) && isset($_GET['type'])) {
+        //echo "Laddar " . $_GET['id'] . "<br>";
+        $type = $_GET['type'];
+        $id = $_GET['id'];
+    } else {
+        
+        header('Location: ../index.php');
+        exit;
+    }
+}
+
+switch ($type) {
+    case "house":
+        $object = House::loadById($id);
+        break;
+//     case "prop":
+//         $object = Prop::loadById($id);
+//         break;
+//     case "npc":
+//         $object = NPC::loadById($id);
+//         break;
+}
+
+
+if (!isset($object)) {
+    header('Location: ../index.php');
+    exit;
+}
+
+$object->ImageId = null;
+$object->update();
+// Image::delete($object->ImageId);
+$object->destroy();
+
+
+
+if (isset($_SERVER['HTTP_REFERER'])) {
+    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    exit;
+}
+
+header('Location: ../index.php?message=image_deleted');
+exit;
