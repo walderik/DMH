52178b78819a62ff03ef257dbb40c7e4641d2cf2
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2024-02-22 12:21:16 +0100

    Länk till resultat

--- admin/commerce.php
+++ admin/commerce.php
@@ -32,7 +32,9 @@ include 'navigation.php';
         <br>
         <a href="reports/titledeeds_info_pdf.php" target="_blank"><i class="fa-solid fa-file-pdf"></i>Lista med lagfarter (för notarie)</a><br>  
         <a href="reports/titledeeds_info_pdf.php?all_info=1" target="_blank"><i class="fa-solid fa-file-pdf"></i>Lista med lagfarter (för handelsansvarig)</a><br><br>  
-        <a href="reports/resources_info_pdf.php?all_info=1" target="_blank"><i class="fa-solid fa-file-pdf"></i>Prislista</a><br><br>  
+        <a href="reports/resources_info_pdf.php?all_info=1" target="_blank"><i class="fa-solid fa-file-pdf"></i>Prislista</a><br>  
+        <br>
+        <a href="titledeed_result_admin.php">Resultat efter lajvet</a><br>
         </p>
 
 </body>
