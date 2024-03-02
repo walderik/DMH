fa7d38df504d55c544f4c94e8569abc3a4158693
Mats Rappe <walderik@yahoo.se>
2024-03-02 17:12:26 +0100

    Mer luft för enklare navigation

--- site-admin/index.php
+++ site-admin/index.php
@@ -6,16 +6,16 @@ include "navigation.php";
 		<div class="content">
 			<h1>Omnes Mundi administration</h1>
 			<p>			    
-				<a href="campaign_admin.php">Kampanjer</a><br>
-			    <a href="house_admin.php">Hus och lägerplatser</a><br>
-			    <a href="user_admin.php">Användare / Logins /Admin behörighet</a><br>
-			    <a href="mail_admin.php">Alla skickade mail</a><br>
-			    <br>
+				<a href="campaign_admin.php">Kampanjer</a><br><br>
+			    <a href="house_admin.php">Hus och lägerplatser</a><br><br>
+			    <a href="user_admin.php">Användare / Logins /Admin behörighet</a><br><br>
+			    <a href="mail_admin.php">Alla skickade mail</a><br><br>
+			    <br><br>
 				<a href="dev-tools/doBackup.php">Ta en backup</a>
 		    </p>
 		    <h2>Basdata</h2>
 		    <p>	    			
-    		    <a href="selection_data_general_admin.php?type=normalallergytypes">Vanliga allergier</a>	<br>
+    		    <a href="selection_data_general_admin.php?type=normalallergytypes">Vanliga allergier</a>	<br><br>
     		    <a href="selection_data_general_admin.php?type=experiences">Erfarenhet som lajvare</a>	<br>
 		    </p>
 		    <?php  if (Dbh::isLocal()) { ?>
