24adc67470b491e01a370401c42a44779a47a3c8
Mats Rappe <walderik@yahoo.se>
2024-03-12 16:08:16 +0100

    Möjlighet mail till tidigare boende i husen

--- site-admin/view_house.php
+++ site-admin/view_house.php
@@ -71,21 +71,21 @@ ul.list {
         			<tr>
         				<td colspan = '2'><h2>Husförvaltare är:</h2><br>
         					<table id='caretakers' class='data'>
-            				<th>Namn</th><th>Medlem</th><th>&nbsp;</th><th>&nbsp;</th>
-            				<?php 
-            				$caretakers = $house->getHousecaretakers();
-            				foreach ($caretakers as $caretaker) {
-            				    $person = $caretaker->getPerson();
-            				    echo "<tr>\n";
-            				    echo "  <td><a href='view_person.php?id=$person->Id'>$person->Name</a></td>\n";
-            				    echo "  <td>".showStatusIcon($caretaker->isMember())."</td>\n";
-            				    echo "  <td>".contactEmailIcon($person->Name, $person->Email)."</td>\n";
-            				    $txt = '"Är du säker '.$person->Name.' inte ska vara husförvaltare?"';
-            				    $confirm = "onclick='return confirm($txt)'";
-            				    echo "  <td><a href='logic/remove_caretaker.php?id=$person->Id&houseId=$house->Id' $confirm><i class='fa-solid fa-trash'></i></a></td>\n";
-            				    echo "</tr>\n";
-            				}
-            				?>
+                				<th>Namn</th><th>Medlem</th><th>Email</th><th>&nbsp;</th>
+                				<?php 
+                				$caretakers = $house->getHousecaretakers();
+                				foreach ($caretakers as $caretaker) {
+                				    $person = $caretaker->getPerson();
+                				    echo "<tr>\n";
+                				    echo "  <td><a href='view_person.php?id=$person->Id'>$person->Name</a></td>\n";
+                				    echo "  <td>".showStatusIcon($caretaker->isMember())."</td>\n";
+                				    echo "  <td>".contactEmailIcon($person->Name, $person->Email)."</td>\n";
+                				    $txt = '"Är du säker '.$person->Name.' inte ska vara husförvaltare?"';
+                				    $confirm = "onclick='return confirm($txt)'";
+                				    echo "  <td><a href='logic/remove_caretaker.php?id=$person->Id&houseId=$house->Id' $confirm><i class='fa-solid fa-trash'></i></a></td>\n";
+                				    echo "</tr>\n";
+                				}
+                				?>
         					</table>
         				</td>
         			</tr>
@@ -98,12 +98,13 @@ ul.list {
             				    echo "<h3>$larp->Name</h3>";
             				    echo substr($larp->StartDate, 2, 8) .' -> '.substr($larp->EndDate, 2, 8)."<br><br>\n";
             					echo "<table id='caretakers' class='data'>";
-                				echo "<th>Namn</th>";
+                				echo "<th>Namn</th><th>Email</th>";
                 				
             				    $personsInHouse = Person::personsAssignedToHouse($house, $larp);
             				    foreach ($personsInHouse as $person) {
                 				    echo "<tr>\n";
-                				    echo "  <td>$person->Name</td>\n";
+                				    echo "  <td><a href='view_person.php?id=$person->Id'>$person->Name</a></td>\n";
+                				    echo "  <td>".contactEmailIcon($person->Name, $person->Email)."</td>\n";
                 				    echo "</tr>\n";
                 				}
                 				echo "</table><br>\n";
