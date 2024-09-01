962b91bcd124c43fa04df262ed48efedfe93b171
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2024-09-01 13:10:10 +0200

    Grupperingar på karaktärbladet

--- pdf/character_sheet_pdf.php
+++ pdf/character_sheet_pdf.php
@@ -32,6 +32,7 @@ class CharacterSheet_PDF extends PDF_MemImage {
     public $person;
     public $isMyslajvare;
     public $larp;
+    public $subdivisions;
     public $all;
     public $current_left;
     public $cell_y_space;        # Standardhöjden på en cell 
@@ -140,9 +141,8 @@ class CharacterSheet_PDF extends PDF_MemImage {
         
         # Kolla först att det finns intriger och att någon har en intrigtext
         $intrigues = Intrigue::getAllIntriguesForRole($this->role->Id, $this->larp->Id);
-        $subdivisions = Subdivision::allForRole($this->role);
         
-        if (empty($intrigues) && empty($subdivisions)) return true;
+        if (empty($intrigues) && empty($this->subdivisions)) return true;
         
         $tomma_intriger = true;
         foreach ($intrigues as $intrigue) {
@@ -153,7 +153,7 @@ class CharacterSheet_PDF extends PDF_MemImage {
             
             if (!empty($intrigueActor->IntrigueText) || !empty($intrigueActor->OffInfo) || (!$isInSubdivision && !empty($intrigue->CommonText))) $tomma_intriger = false;
         }
-        foreach ($subdivisions as $subdivision) {
+        foreach ($this->subdivisions as $subdivision) {
             $subdivisionIntrigues = Intrigue::getAllIntriguesForSubdivision($subdivision->Id, $this->larp->Id);
             foreach ($subdivisionIntrigues as $intrigue) {
                 if (!$intrigue->isActive()) continue;
@@ -165,7 +165,11 @@ class CharacterSheet_PDF extends PDF_MemImage {
         if ($tomma_intriger) return true;
   
         if ($new_page) $this->AddPage();
-        else $y = $this->GetY()+$space*3;
+        else {
+            $y = $this->GetY()+$space*1;
+            $this->bar();
+            $y = $this->GetY()+$space*3;
+        }
         $this->SetXY($left, $y);
         $this->SetFont('Helvetica','B',static::$text_fontsize);
         $this->Cell($this->cell_width, static::$cell_y, encode_utf_to_iso('Intriger'),0,0,'L');
@@ -203,7 +207,7 @@ class CharacterSheet_PDF extends PDF_MemImage {
         $known_props = $this->role->getAllKnownProps($this->larp);
         
 
-        foreach ($subdivisions as $subdivision) {
+        foreach ($this->subdivisions as $subdivision) {
             $subdivisionIntrigues = Intrigue::getAllIntriguesForSubdivision($subdivision->Id, $this->larp->Id);
             foreach ($subdivisionIntrigues as $intrigue) {
                 if ($intrigue->isActive()) {
@@ -384,6 +388,57 @@ class CharacterSheet_PDF extends PDF_MemImage {
         $this->SetXY($this->current_left, $y);
     }
     
+    protected function subdivision_info(){
+        if (!empty($this->subdivisions)) {
+            foreach ($this->subdivisions as $subdivision) {
+                if ($subdivision->isVisibleToParticipants()) {
+                    if ($subdivision->canSeeOtherParticipants()) $this->print_open_subdivision($subdivision);
+                    else $this->print_secret_subdivision($subdivision);
+                }
+            }
+        }
+
+    }
+    
+    function print_open_subdivision(Subdivision $subdivision) {
+        global $y, $left;
+        $space = 3;
+        $this->print_secret_subdivision($subdivision);
+
+        $y = $this->GetY()+$space*2;
+        $membernames = array();
+        foreach ($subdivision->getAllRegisteredMembers($this->larp) as $role) {
+            $membernames[] = $role->Name;
+        }
+     
+        if (!empty($membernames)) {
+            $this->SetXY($left, $y);
+            $this->Cell($this->cell_width, static::$cell_y, encode_utf_to_iso("Medlemmar:"),0,0,'L');
+            $y = $this->GetY()+$space*2;
+            $this->SetXY($left, $y);
+            $this->MultiCell(0, static::$cell_y-1, trim(encode_utf_to_iso(implode(", ", $membernames))), 0, 'L');
+            
+        }
+        
+    }
+    
+    function print_secret_subdivision(Subdivision $subdivision) {
+        global $y, $left;
+        $space = 3;
+        $y = $this->GetY()+$space;
+        $this->bar();
+        $y = $this->GetY()+$space*2;
+        $this->SetFont('Helvetica','B',static::$text_fontsize);
+        $this->SetXY($left, $y);
+        $this->Cell(0, static::$cell_y, encode_utf_to_iso($subdivision->Name),0,0,'L');
+        $this->SetFont('Arial','',static::$text_fontsize-3);
+        $y = $this->GetY()+$space*2;
+        $this->SetXY($left, $y);
+        $this->MultiCell(0, static::$cell_y-1, trim(encode_utf_to_iso($subdivision->Description)), 0, 'L');
+        
+    }
+    
+    
     function rumours($new_page = true){
         global $y, $left;
         $space = 3;
@@ -495,8 +550,8 @@ class CharacterSheet_PDF extends PDF_MemImage {
                     }
                 }
                 
-                $subdivisions = Subdivision::allForRole($this->role);
-                foreach ($subdivisions as $subdivision) {
+                
+                foreach ($this->subdivisions as $subdivision) {
                     $subdivisionIntrigues = Intrigue::getAllIntriguesForSubdivision($subdivision->Id, $prevoius_larp->Id);
                     foreach ($subdivisionIntrigues as $intrigue) {
                         if ($intrigue->isActive()) {
@@ -584,6 +639,7 @@ class CharacterSheet_PDF extends PDF_MemImage {
         $this->person = $this->role->getPerson();
         $this->isMyslajvare = $this->role->isMysLajvare();
         $this->larp = $larp_in;
+        $this->subdivisions = Subdivision::allForRole($this->role);
         $this->cell_y_space = static::$cell_y + (2*static::$Margin);
         $this->current_cell_height = $this->cell_y_space;
         
@@ -610,6 +666,7 @@ class CharacterSheet_PDF extends PDF_MemImage {
         $this->bar();
         
         $y += 3;
+
         
         if (!$this->isMyslajvare && !$this->isReserve) {
             $this->intrigues(false);
@@ -625,6 +682,7 @@ class CharacterSheet_PDF extends PDF_MemImage {
         $this->person = $this->role->getPerson();
         $this->isMyslajvare = $this->role->isMysLajvare();
         $this->larp = $larp_in;
+        $this->subdivisions = Subdivision::allForRole($this->role);
         $this->all = $all_information;
         $this->cell_y_space = static::$cell_y + (2*static::$Margin);
         $this->current_cell_height = $this->cell_y_space;
@@ -684,7 +742,9 @@ class CharacterSheet_PDF extends PDF_MemImage {
         
         $this->beskrivning();
 
-
+        
+        $this->subdivision_info();
+        
         if (!$this->isMyslajvare && !$this->isReserve && ($this->larp->isIntriguesReleased() || $this->all)) {
             $this->intrigues(false);
             $this->rumours(false);
