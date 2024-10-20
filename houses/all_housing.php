e26853b5d4c5db00d2e5453561907717d7da1b9f
Mats Rappe <walderik@yahoo.se>
2024-10-20 12:16:40 +0200

    Bilder och länk på lägerplatskartan

--- houses/all_housing.php
+++ houses/all_housing.php
@@ -55,7 +55,14 @@ foreach ($houses as $house) {
         echo "var target = L.latLng('$house->Lat', '$house->Lon');";
         
         echo "marker = L.marker(target).addTo(map);";
-        echo "marker.bindPopup('$house->Name').openPopup();";
+        
+        $text = "<a href='view_house.php?operation=update&id=$house->Id' target='_blank'><b>$house->Name</b></a><br>";
+        
+        if ($house->hasImage()) {
+            $text .= "<br><img width='100px' height='100px' alt='House image' src='../includes/display_image.php?id=$house->ImageId'/>";
+        }      
+        
+        echo "marker.bindPopup(\"$text\").openPopup();";
 
     }
 }
