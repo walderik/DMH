57b5dfedc59f2a0d2f633b50ad4b37988dfc2c25
Cornelia Karlslund <cornelia.karlslund@ica.se>
2024-11-22 15:49:02 +0100

    SortTable -> sortTableNumbers

--- admin/groups.php
+++ admin/groups.php
@@ -39,7 +39,7 @@ th {
         		    "<th>&nbsp; &nbsp; </th>";
     		    $colnum++;
     		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Gruppledare</th>";
-				echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Antal medlemmar</th>";
+				echo "<th onclick='sortTableNumbers(". $colnum++ .", \"$tableId\")'>Antal medlemmar</th>";
     		    if (Wealth::isInUse($current_larp)) echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Rikedom</th>";
     		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")' colspan='2'>Intrig</th></tr>\n";
     		    foreach ($groups as $group)  {
