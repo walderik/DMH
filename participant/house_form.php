ff020580f1ceaf0fbee04e5ee9d7f75badcbe3dc
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2024-12-11 14:02:12 +0100

    Husbrev

--- participant/house_form.php
+++ participant/house_form.php
@@ -27,38 +27,26 @@ img {
 }
 </style>
 
-    <div class="content"> 
-    	<h1>Ändra husbrevet för <?php echo $house->Name ?></h1>
-    	<div style="width:60%";>Husbrevet är en text du som husförvaltare skriver till de som skall bo i huset.<br>
-    	Det skickas till alla som placeras i huset på något av Berghems lajv.<br>
-    	Det bör innehålla information om vad man får och inte får göra i huset och hur huset fungerar på olika sätt.<br>
-    	Alla huseförvaltare har rätt att redigera den här texten.</div>
+	<div class='itemselector'>
+	<div class="header">
+
+		<i class="fa-solid fa-people-group"></i>
+		Ändra husbrevet för <?php echo $house->Name ?>
+	</div>
+
     	<form action="view_house.php?id=<?php echo $house->Id ?>&operation=updateNotesToUser" method="post">
     		<input type="hidden" id="operation" name="operation" value="updateNotesToUser"> 
-    		<table>
-        		<tr>
-            		<td>
-            		<table>
 
+    	<div class='itemcontainer'>
+    	Husbrevet är en text du som husförvaltare skriver till de som skall bo i huset.<br>
+    	Det skickas till alla som placeras i huset på något av Berghems lajv.<br>
+    	Det bör innehålla information om vad man får och inte får göra i huset och hur huset fungerar på olika sätt.<br>
+    	Alla husförvaltare har rätt att redigera den här texten.<br>
+    	<textarea id="NotesToUsers" name="NotesToUsers" rows="17" cols="90" maxlength="600000" required><?php echo htmlspecialchars($house->NotesToUsers); ?></textarea>
+      	</div>
+  	
+  		<div class='center'><input id="submit_button" class='button-18' type="submit" value="Ändra"></div>
 
-            			<?php if ($house->IsHouse()) {?>
-            			<tr>
-            				<td>
-            					<textarea id="NotesToUsers" name="NotesToUsers" rows="17" cols="90" maxlength="600000" required><?php echo htmlspecialchars($house->NotesToUsers); ?></textarea>
-            				</td>
-            			</tr>
-            			
-            			<tr>
-            				<td></td><td><input id="submit_button" type="submit" value="Ändra"></td>
-            			</tr>
-            			<tr>
-            				<td>&nbsp;</td>
-            			</tr>
-            			<?php }?>
-            		</table>
-            		</td>
-    			</tr>
-			</table>
     	</form>
     	</div>
     </body>
