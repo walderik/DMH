8a3eece77da188d3f0e979610e5cebc50c78651c
Cornelia Karlslund <cornelia.karlslund@ica.se>
2025-01-02 17:22:15 +0100

    Basic intrigue creator page

--- admin/intrigue_creator_page.php
+++ admin/intrigue_creator_page.php
@@ -9,105 +9,63 @@ th {
 }
 </style>
 
-<script src="../javascript/table_sort.js"></script>
+<body>
+    <script src="../javascript/table_sort.js"></script>
 
-    <div class="content">   
-        <h1>Intrigskapare</h1>
+        <div class="content">   
+            <h1>Intrigskapare</h1>
 
-        <?php 
-        $characters = $current_larp->getAllMainRoles(false);
+            <?php 
+            $characters = $current_larp->getAllMainRoles(false);
 
-        if (empty($characters)) {
-            echo "Inga anmälda karaktärer";
-        } else {
-        $tableId = "characters";
-        $colnum = 0;
+            if (empty($characters)) {
+                echo "Inga anmälda karaktärer";
+            } else {
+            $tableId = "characters";
+            $colnum = 0;
 
-        echo "<table id='$tableId' class='data'>";
-        echo "<tr>";
-        echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Karaktär</th>";
+            echo "<table id='$tableId' class='data'>";
+            echo "<tr>";
+            echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Karaktär</th>";
+            echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Intrigidéer</th>";
+            echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Mörk Hemlighet</th>";
+            echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Mörk Hemlighet idéer</th>";
+            // echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Anteckningar</th>";
+            
+            foreach ($characters as $character) {
+                echo "<tr>";
+                echo "<td>";
+                echo $character->getViewLink();
+                $group = $character->getGroup();
+                if (!is_null($group)){
+                    echo "<br>";
+                    echo $group->getViewLink();
+                }
+                echo "</td\n>";
 
-        
-        foreach ($characters as $character) {
-            echo "<tr>\n";
-            echo "<td>";
-            echo $character->getViewLink();
-            echo "</td\n>";
+                echo "<td>";
+                echo $character->IntrigueSuggestions;
+                echo "</td>\n";
+
+                echo "<td>";
+                echo $character->DarkSecret;
+                echo "</td>\n";
+
+                echo "<td>";
+                echo $character->DarkSecretIntrigueIdeas;
+                echo "</td>\n";
+                
+                echo "</tr>";
+
+                //TODO
+                // echo "<td>";
+                // echo $character->OrganizerNotes;
+                // echo "</td>\n";
+                }
+            
+                echo "</table>";
             }
-        
-
-        // $groups = Group::getAllRegistered($current_larp);
-        // if (empty($groups)) {
-        //     echo "Inga anmälda grupper";
-        // } else {
-        //     $emailArr = array();
-        //     foreach ($groups as $group) {
-        //         $person = $group->getPerson();
-        //         $personIdArr[] = $person->Id;
-        //     }
-            
-        //     echo contactSeveralEmailIcon('Skicka till gruppledarna', $personIdArr, 'Gruppledare', "Meddelande till alla gruppledarna i $current_larp->Name");
-            
-        //     $tableId = "groups";
-        //     $colnum=0;
-        //     echo "<table id='$tableId' class='data'>";
-        //     echo "<tr><th onclick='sortTable(". $colnum++ .", \"$tableId\");'>Namn</th>".
-        //         "<th>&nbsp; &nbsp; </th>";
-        //     $colnum++;
-        //     echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Gruppledare</th>";
-        //     echo "<th onclick='sortTableNumbers(". $colnum++ .", \"$tableId\")'>Antal medlemmar</th>";
-        //     if (Wealth::isInUse($current_larp)) echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Rikedom</th>";
-        //     echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")' colspan='2'>Intrig</th></tr>\n";
-        //     foreach ($groups as $group)  {
-        //         echo "<tr>\n";
-        //         echo "<td>";
-        //         echo $group->getViewLink();
-        //         if ($group->userMayEdit($current_larp)) echo "<br>Gruppledaren får ändra gruppen " . showStatusIcon(false);
-                
-        //         echo "</td>\n";
-        //         echo "<td>";
-        //         echo $group->getEditLinkPen(true);
-        //         echo "\n";
-        //         echo "<a href='group_sheet.php?id=" . $group->Id . "' target='_blank'><i class='fa-solid fa-file-pdf' title='Gruppblad för $group->Name'></i></a>\n";
-        //         echo "</td>\n";
-        //         $person = $group->getPerson();
-        //         echo "<td>";
-        //         $registration = $person->getRegistration($current_larp);
-        //         if (isset($registration) && !$registration->isNotComing()) echo $person->Name;
-        //         elseif (!isset($registration)) {
-        //             $reserveregistration = Reserve_Registration::loadByIds($person->Id, $current_larp->Id);
-        //             if (isset($reserveregistration)) echo "<s>$person->Name</s> (på reservlistan)";
-        //             else echo "<s>$person->Name</s> (inte anmäld)";
-        //         }
-        //         else echo "<s>$person->Name</s> (avbokad)";
-        //         echo " " . contactEmailIcon($person) . "</td>\n";
-        //         echo "<td>" . $group->countAllRolesInGroup($current_larp) . "</td>\n";
-        //         if (Wealth::isInUse($current_larp)) echo "<td>" . $group->getWealth()->Name . "</td>\n";
-        //         $larp_group = LARP_Group::loadByIds($group->Id, $current_larp->Id);
-        //         echo "<td>" . showStatusIcon($group->hasIntrigue($current_larp));
-        //         $intrigueWords = $group->intrigueWords($current_larp);
-                
-        //         if (!empty($intrigueWords)) echo "<br>$intrigueWords ord";
-        //         $intrigues = Intrigue::getAllIntriguesForGroup($group->Id, $current_larp->Id);
-        //         echo "<br>";
-        //         if (!empty($intrigues)) echo "Intrig: ";
-        //         foreach ($intrigues as $intrigue) {
-        //             echo "<a href='view_intrigue.php?Id=$intrigue->Id'>";
-        //             if ($intrigue->isActive()) echo $intrigue->Number;
-        //             else echo "<s>$intrigue->Number</s>";
-        //             echo "</a>";
-        //             echo " ";
-        //         }
-                
-        //         echo "</td>\n";
-        //         echo "<td><a href='edit_group_intrigue.php?id=" . $group->Id . "'><i class='fa-solid fa-pen'></i></a>\n";
-        //         echo "</td>";
-        //         echo "</tr>\n";
-        //     }
-            echo "</table>";
-        }
-        ?>
-	</div>
-</body>
-
+            ?>
+        </div>
+    </body>
 </html>
