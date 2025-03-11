0ecbeccadc29ccaf85a2bf1791981570daf9ad30
Cornelia Karlslund <cornelia.karlslund@ica.se>
2025-03-11 08:20:05 +0100

    Add titleDeed to group page

--- admin/groups.php
+++ admin/groups.php
@@ -40,7 +40,11 @@ th {
     		    $colnum++;
     		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Gruppledare</th>";
 				echo "<th onclick='sortTableNumbers(". $colnum++ .", \"$tableId\")'>Antal medlemmar</th>";
-    		    if (Wealth::isInUse($current_larp)) echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Rikedom</th>";
+    		    if (Wealth::isInUse($current_larp)) 
+				{ 
+					echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Rikedom</th>"; 
+					echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Har verksamhet</th>"; 
+				}
     		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")' colspan='2'>Intrig</th></tr>\n";
     		    foreach ($groups as $group)  {
     		        echo "<tr>\n";
@@ -66,7 +70,12 @@ th {
     		        else echo "<s>$person->Name</s> (avbokad)";
     		        echo " " . contactEmailIcon($person) . "</td>\n";
 					echo "<td>" . $group->countAllRolesInGroup($current_larp) . "</td>\n";
-    		        if (Wealth::isInUse($current_larp)) echo "<td>" . $group->getWealth()->Name . "</td>\n";
+    		        if (Wealth::isInUse($current_larp)) {
+						echo "<td>" . $group->getWealth()->Name . "</td>\n";
+						$titledeeds = Titledeed::getAllForGroup($group);
+						$hasTitleDeed = empty($titledeeds) ? 'Nej' : 'Ja';
+						echo "<td>" . $hasTitleDeed . "</td>\n";
+					}
     		        $larp_group = LARP_Group::loadByIds($group->Id, $current_larp->Id);
     		        echo "<td>" . showStatusIcon($group->hasIntrigue($current_larp));
     		        $intrigueWords = $group->intrigueWords($current_larp);
