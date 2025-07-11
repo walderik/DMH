c2cfe1eebc21f911edc951884eda739d7a5c9b19
Mats Rappe <119478948+Pliblib@users.noreply.github.com>
2025-07-11 16:30:45 +0200

    Kommenterar bort så länge

--- classes/environment.php
+++ classes/environment.php
@@ -26,13 +26,14 @@ class Environment {
     }
     
     public static function startSession() {
-        if (!isset($_SESSION['CREATED'])) {
+/*       if (!isset($_SESSION['CREATED'])) {
             $_SESSION['CREATED'] = time();
         } else if (time() - $_SESSION['CREATED'] > 1800) {
             // session started more than 30 minutes ago
             session_regenerate_id(true);    // change session ID for the current session and invalidate old session ID
             $_SESSION['CREATED'] = time();  // update creation time
         }
+*/
         session_start();
     }
     
