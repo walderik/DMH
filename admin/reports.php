3457ee66233f657490466e55b2bb5d1a2c4a6997
Pliblib <karin@tellen.se>
2025-09-18 11:40:59 +0200

    Ta bort länkar som inte finns längre

--- admin/reports.php
+++ admin/reports.php
@@ -104,8 +104,8 @@ include 'navigation.php';
 	</div>
     <h3>Ekonomi</h3>
 	<div class='linklist'>
-        <a href="reports/economy.php" target="_blank"><i class="fa-solid fa-file-pdf"></i>Rapport till kassören</a><br>
-        <a href="logic/all_bookkeeping_zip.php" target="_blank"><i class="fa-solid fa-file-zipper"></i> Alla verifikationer till kassör</a><br>
+        <s><i class="fa-solid fa-file-pdf"></i>Rapport till kassören</s> (Görs för kampanjen)<br>
+        <s><i class="fa-solid fa-file-zipper"></i> Alla verifikationer till kassör</s> (Görs för kampanjen)<br>
 		<a href="reports/fees_payed.php" target="_blank"><i class="fa-solid fa-file-pdf"></i>Inbetalade och återbetalade deltagaravgifter</a><br>
         
 	</div>
