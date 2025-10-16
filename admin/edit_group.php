b9f72c1c44299c5df2ce5685a31ebfa7f8573a75
Pliblib <karin@tellen.se>
2025-10-16 16:43:12 +0200

    Tydligare text

--- admin/edit_group.php
+++ admin/edit_group.php
@@ -249,7 +249,7 @@ include 'navigation.php';
 				<input type="radio" id="Visibility_Not_Choose" name="Visibility" value="<?php echo Group::VISIBILITY_NOT_CHOOSE?>" onclick="handleVisibility()" <?php if ($group->hasNotChooseVisibility()) echo 'checked="checked"'?>> 
     			<label for="Visibility_Not_Choose">Synlig NPC-grupp. Visas i listor över tex deltagare, men deltagare kan inte välja den för sina karaktärer</label><br> 
     			<input type="radio" id="Visibility_Invisible" name="Visibility" value="<?php echo Group::VISIBILITY_INVISIBLE?>" onclick="handleVisibility()" <?php if ($group->hasInvisibility()) echo 'checked="checked"'?>> 
-    			<label for="Visibility_Invisible">Osynligt NPC-grupp. Visas inte för deltagare, mer än om de har en karaktär i gruppen</label>
+    			<label for="Visibility_Invisible">Osynligt NPC-grupp. Visas inte för deltagare i tex NPC-galleri. Spelaren kan se den om de har en karaktär i gruppen, eller har den som "känner till".</label>
 			</td></tr>
 
 			<tr><td valign="top" class="header">Död/Ej i spel&nbsp;<font style="color:red">*</font></td>
