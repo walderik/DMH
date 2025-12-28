baee4a9305c03884c4b6632ce4272461479c9318
Mats Rappe <mats@fraktjakt.se>
2025-12-28 14:01:49 +0100

    Hjälptexter

--- admin/npc.php
+++ admin/npc.php
@@ -9,7 +9,15 @@ include 'npc_navigation.php';
 
     <div class="content">   
         <h1>NPC</h1>
-        
+        En NPC är en roll som inte ägs och kontrolleras av en deltagare. Den kan tilldelas en deltagare och blir då en "karaktär" som den deltagare får hantera precis som sina andra karaktärer. <br>
+        Tills dess en NPC har en deltagare kontrolleras den av arrangörerna. <br>
+        <br>
+        En NPC kan tillfälligt under ett lajv tilldelas att spelas av en deltagare i ett NPC-Uppdrag. Det är då ett tillfälligt inhopp av någon deltagare bara på det lajvet och vanligtivs med tydliga instruktioner av arrangörerna.<br>
+        Om en NPC skall spelas under ett lajv, måste man först skapa ett Uppdrag som man sedan kan tilldelas en deltagare som har anmält att de vill spela NPC.<br>
+        <br>
+        En NPC kan tillhöra en vanlig grupp, precis som vilken annan roll som helst, men den kan också tillhöra grupper utan deltagare. Dessa grupper kan vara synliga eller dolda för övriga deltagare.<br>
+        <br>
+        Vanliga deltagare kan skapa NPC:er till sina grupper. Dessa måste då också godkännas av arrangörerna innan de tillhör fiktionen.
         <p>
         <?php 
         $notcoming_count = NPC_assignment::numberNotComingNPCPlayers($current_larp);
@@ -26,9 +34,6 @@ include 'npc_navigation.php';
         $numberInterestedUnassigned = NPC_assignment::numberInterestedUnassigned($current_larp);
         
         ?>
-        
-        
-
 
         <?php echo count($npcs) ?> NPC'er kommer att spelas på lajvet. <br><br>
         <?php echo "Det finns ".count($persons)." deltagare som vill spela NPC'er"; ?>
--- admin/npc_overview.php
+++ admin/npc_overview.php
@@ -1,10 +1,6 @@
 <?php
 include_once 'header.php';
 
-
-
-
-
 include 'navigation.php';
 include 'npc_navigation.php';
 
@@ -17,25 +13,24 @@ include 'npc_navigation.php';
         <h1>Alla NPC</h1>
 
 			<?php 
-			echo "NPC'er filtrerade på levande<br><br>";
-			echo '<button id="btn_show" onclick="show_hide();">Visa alla</button>';
-			echo "<br><br>";
+			echo "NPC'er filtrerade på levande<br><br>\n";
+			echo '<button id="btn_show" onclick="show_hide();">Visa alla</button>\n';
+			echo "<br><br>\n";
 			
 		    $tableId = "npc_roles";
 		    $colnum = 0;
-		    echo "<table id='$tableId' class='data'>";
-		    echo "<tr>";
-		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\");'>Namn</th>";
-		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\");'>Godkänd NPC</th>";
-		    
-		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Grupptillhörighet</th>";
-		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Tilldelad i ett uppdrag</th>";
+		    echo "<table id='$tableId' class='data'>\n";
+		    echo "<tr>\n";
+		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\");'>Namn</th>\n";
+		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\");'>Godkänd NPC</th>\n";
+		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Grupptillhörighet</th>\n";
+		    echo "<th onclick='sortTable(". $colnum++ .", \"$tableId\")'>Tilldelad i ett uppdrag</th>\n";
 		    echo "</tr>";
 
 			$npcs = Role::getAllNPC($current_larp);
 			foreach ($npcs as $npc) {
 			    if ($npc->IsDead) echo "<tr class='show_hide hidden'>\n";
-			    else echo "<tr>";
+			    else echo "<tr>\n";
 			    
 			    echo "<td>".$npc->getViewLink()." ".$npc->getEditLinkPen(true);
 			    
@@ -43,41 +38,36 @@ include 'npc_navigation.php';
 			        echo "&nbsp;<a href='logic/delete_role.php?id=" . $npc->Id . "'><i class='fa-solid fa-trash' title='Ta bort karaktär'></i></a>";
 			    }
 			    
-			    echo "</td>";
+			    echo "</td>\n";
 			    
-			    echo "<td>";
+			    echo "<td>\n";
 			    if ($npc->isApproved()) echo showStatusIcon(true);
 			    elseif ($npc->userMayEdit()) echo showStatusIcon(false)."<br>Spelare får ändra på karaktären och därför kan den inte godkännas.";
-			    else {
-			        echo "<form action='logic/approve.php' method='post'>";
-			        echo "<input type='hidden' id='RoleId' name='RoleId' value='$npc->Id'>";
-			        echo "<button class='invisible' type='submit'>".showStatusIcon(false,NULL,NULL,'Klick')."</button>";
-			        echo "</form>";
-			    }
+			    else echo showPostStatusIcon(false, 'logic/approve.php', null, 'Klicka för att godkänna NPC', NULL, ['RoleId'=>$npc->Id], null);
+
 			    $group = $npc ->getGroup();
 			    if (!empty($group)) {
-			        echo "<td>".$group->getViewLink().' '.$group->getVisibilityText()."</td>";
-			    } else echo "<td></td>";
+			        echo "<td>".$group->getViewLink().' '.$group->getVisibilityText()."</td>\n";
+			    } else echo "<td></td>\n";
 			    
-			    echo "<td>";
+			    echo "<td>\n";
 			    $assignement = NPC_assignment::getAssignment($npc, $current_larp);
 			    if (empty($assignement)) {
-			        echo "<form action='npc_form.php' method='POST'><input type='hidden' id='roleId' name='roleId' value='$npc->Id'>";
-			        echo "<input type='hidden' id='operation' name='operation' value='insert'><button class='invisible' type='submit'>".showStatusIcon(false)."</button></form>";
+			        echo showPostStatusIcon(false, 'npc_form.php', null, 'Skapa NPC-Uppdrag', 'Har uppdrag', ['roleId'=>$npc->Id, 'operation'=>'insert'], null);
 			    } elseif ($assignement->isAssigned()) {
 			        echo showStatusIcon(true)." ";
 			        $person = $assignement->getPerson();
 			        $registration = $person->getRegistration($current_larp);
-			        if ($registration->isNotComing()) echo "<s>".$person->getViewLink()."</s> ".showStatusIcon(false);
+			        if ($registration->isNotComing()) echo "<s>".$person->getViewLink()."</s>  ".showStatusIcon(false, NULL, NULL, 'Kommer inte på lajvet');
 			        else echo $person->getViewLink();
 			    } else {
-			        echo "<form action='npc_form.php' method='POST'><input type='hidden' id='roleId' name='roleId' value='$npc->Id'>";
-			        echo "<input type='hidden' id='operation' name='operation' value='update'><button class='invisible' type='submit'>".showWarningIcon()."</button></form>";
+			        echo "<form action='npc_form.php' method='POST'><input type='hidden' id='roleId' name='roleId' value='$npc->Id'>\n";
+			        echo "<input type='hidden' id='operation' name='operation' value='update'><button class='invisible' type='submit'>".showWarningIcon('Saknar spelare')."</button></form>\n";
 			    }
 			    
-			    echo "</td>";
+			    echo "</td>\n";
 			    
-			    echo "<tr>";
+			    echo "<tr>\n";
 			}
 			echo "</table>";
 			
